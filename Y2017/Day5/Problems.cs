0a779583913ffdfb7eecb70d0f88080921a1b64e
Matias Jansson <matias.Jansson@datscha.com>
2017-12-04 18:11:40 +0100

    Preparing for day 5 2017

---
+++ Y2017/Day5/Problems.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Y2017.Day5 {
+    public class Problems {
+
+        private readonly ITestOutputHelper _output;
+
+        public Problems(ITestOutputHelper output) {
+            _output = output;
+        }
+
+        [Fact]
+        public void Problem1() {
+            string[] input = File.ReadAllLines(@".\Day5\input.txt").Select(x => Regex.Replace(x, @"\s+", " ")).ToArray();
+
+            var result = "Not implemented yet";
+
+            _output.WriteLine($"Day 4 problem 1: {result}");
+        }
+
+        [Fact]
+        public void Problem2() {
+            string[] input = File.ReadAllLines(@".\Day5\input.txt").Select(x => Regex.Replace(x, @"\s+", " ")).ToArray();
+
+            var result = "Not implemented yet";
+
+            _output.WriteLine($"Day 4 problem 2: {result}");
+        }
+    }
+}
