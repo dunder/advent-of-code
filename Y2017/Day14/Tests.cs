e404f84d7c4bb887d8f599f68bae1c165cd3de80
Matias Jansson <matias.Jansson@datscha.com>
2017-12-14 22:08:49 +0100

    Day 14 2017 completed

--- Y2017/Day14/Tests.cs
+++ Y2017/Day14/Tests.cs
@@ -4,7 +4,21 @@ namespace Y2017.Day14 {
     public class Tests {
         [Fact]
         public static void Problem1_Example() {
+            const string input = "flqrgnkx";
 
+            var lit = HashGrid.CountLit(input);
+
+            Assert.Equal(8108, lit);
         }
+        [Fact]
+        public static void Problem2_Example() {
+            const string input = "flqrgnkx";
+
+            var groups = HashGrid.ContinousRegions(input);
+
+            Assert.Equal(1242, groups);
+        }
+
+        // 1242
     }
 }
