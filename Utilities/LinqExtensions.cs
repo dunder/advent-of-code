5ec595812decea7c5390196610cb635634a09f9e
Matias Jansson <Matias.Jansson@datscha.com>
2018-10-17 07:16:28 +0200

    Work in progress 2016 day 11

---
+++ Utilities/LinqExtensions.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Utilities {
+    public static class LinqExtensions {
+        public static IEnumerable<T> Yield<T>(this T item) {
+            yield return item;
+        }
+    }
+}
