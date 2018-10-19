80d924550f53f0b6b2bcc016e65b44f74c255a91
Matias Jansson <Matias.Jansson@datscha.com>
2018-10-19 10:18:17 +0200

    All problems moved to cleaner structure

---
+++ Solutions/Event2017/Day24/Tests.cs
@@ -0,0 +1,61 @@
+﻿using Xunit;
+
+namespace Solutions.Event2017.Day24
+{
+    public class Tests
+    {
+        [Fact]
+        public static void FirstStarExample()
+        {
+            string[] input =
+            {
+                "0/2",
+                "2/2",
+                "2/3",
+                "3/4",
+                "3/5",
+                "0/1",
+                "10/1",
+                "9/10"
+            };
+
+            var result = ElectromagneticMoat.StrongestBridge(input);
+
+            Assert.Equal(31, result);
+        }
+
+        [Fact]
+        public static void SecondStarExample()
+        {
+            string[] input =
+            {
+                "0/2",
+                "2/2",
+                "2/3",
+                "3/4",
+                "3/5",
+                "0/1",
+                "10/1",
+                "9/10"
+            };
+
+            var result = ElectromagneticMoat.StrongestLongestBridge(input);
+
+            Assert.Equal(19, result);
+        }
+
+        [Fact]
+        public static void FirstStar()
+        {
+            var actual = new Problem().FirstStar();
+            Assert.Equal("1695", actual);
+        }
+
+        [Fact]
+        public static void SecondStar()
+        {
+            var actual = new Problem().SecondStar();
+            Assert.Equal("1673", actual);
+        }
+    }
+}
---
+++ Solutions/Event2017/Day25/Problem.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Solutions.Event2017.Day25
+{
+    public class Problem : ProblemBase
+    {
+        public override Event Event => Event.Event2017;
+        public override Day Day => Day.Day25;
+
+        public override string FirstStar()
+        {
+            var states = TheHaltingProblem.BuildStates();
+            var result = TheHaltingProblem.DiagnosticChecksum("A", 12317297, states);
+            return result.ToString();
+        }
+
+        public override string SecondStar()
+        {
+            return "No problem to solve";
+        }
+    }
+
+    public class TheHaltingProblem {
+        public static int DiagnosticChecksum(string startState, int iterations, Dictionary<string, Func<StateContext, StateContext>> input) {
+
+            var stateContext = new StateContext(iterations / 2 + 1, new bool[2 * iterations], startState);
+            for (int i = 0; i < iterations; i++) {
+                stateContext = input[stateContext.NextState](stateContext);
+            }
+
+            return stateContext.Tape.Count(b => b);
+        }
+
+        public static Dictionary<string, Func<StateContext, StateContext>> BuildStates() {
+            var states = new Dictionary<string, Func<StateContext, StateContext>>();
+
+            StateContext StateA(StateContext stateContext) {
+                int nextPosition;
+                string nextState;
+
+                if (!stateContext.Tape[stateContext.Position]) {
+                    stateContext.Tape[stateContext.Position] = true;
+                    nextPosition = stateContext.Position + 1;
+                    nextState = "B";
+
+                } else {
+                    stateContext.Tape[stateContext.Position] = false;
+                    nextPosition = stateContext.Position - 1;
+                    nextState = "D";
+                }
+
+                return new StateContext(nextPosition, stateContext.Tape, nextState);
+            }
+
+            StateContext StateB(StateContext stateContext) {
+                int nextPosition;
+                string nextState;
+
+                if (!stateContext.Tape[stateContext.Position]) {
+                    stateContext.Tape[stateContext.Position] = true;
+                    nextPosition = stateContext.Position + 1;
+                    nextState = "C";
+
+                } else {
+                    stateContext.Tape[stateContext.Position] = false;
+                    nextPosition = stateContext.Position + 1;
+                    nextState = "F";
+                }
+
+                return new StateContext(nextPosition, stateContext.Tape, nextState);
+            }
+
+            StateContext StateC(StateContext stateContext) {
+                int nextPosition;
+                string nextState;
+
+                if (!stateContext.Tape[stateContext.Position]) {
+                    stateContext.Tape[stateContext.Position] = true;
+                    nextPosition = stateContext.Position - 1;
+                    nextState = "C";
+
+                } else {
+                    stateContext.Tape[stateContext.Position] = true;
+                    nextPosition = stateContext.Position - 1;
+                    nextState = "A";
+                }
+
+                return new StateContext(nextPosition, stateContext.Tape, nextState);
+            }
+
+            StateContext StateD(StateContext stateContext) {
+                int nextPosition;
+                string nextState;
+
+                if (!stateContext.Tape[stateContext.Position]) {
+                    stateContext.Tape[stateContext.Position] = false;
+                    nextPosition = stateContext.Position - 1;
+                    nextState = "E";
+
+                } else {
+                    stateContext.Tape[stateContext.Position] = true;
+                    nextPosition = stateContext.Position + 1;
+                    nextState = "A";
+                }
+
+                return new StateContext(nextPosition, stateContext.Tape, nextState);
+            }
+
+            StateContext StateE(StateContext stateContext) {
+                int nextPosition;
+                string nextState;
+
+                if (!stateContext.Tape[stateContext.Position]) {
+                    stateContext.Tape[stateContext.Position] = true;
+                    nextPosition = stateContext.Position - 1;
+                    nextState = "A";
+
+                } else {
+                    stateContext.Tape[stateContext.Position] = false;
+                    nextPosition = stateContext.Position + 1;
+                    nextState = "B";
+                }
+
+                return new StateContext(nextPosition, stateContext.Tape, nextState);
+            }
+
+            StateContext StateF(StateContext stateContext) {
+                int nextPosition;
+                string nextState;
+
+                if (!stateContext.Tape[stateContext.Position]) {
+                    stateContext.Tape[stateContext.Position] = false;
+                    nextPosition = stateContext.Position + 1;
+                    nextState = "C";
+
+                } else {
+                    stateContext.Tape[stateContext.Position] = false;
+                    nextPosition = stateContext.Position + 1;
+                    nextState = "E";
+                }
+
+                return new StateContext(nextPosition, stateContext.Tape, nextState);
+            }
+
+            states.Add("A", StateA);
+            states.Add("B", StateB);
+            states.Add("C", StateC);
+            states.Add("D", StateD);
+            states.Add("E", StateE);
+            states.Add("F", StateF);
+
+            return states;
+        }
+
+        private static void Print(StateContext stateContext) {
+            for (int x = 0; x < stateContext.Tape.Length; x++) {
+                if (x == stateContext.Position) {
+                    Console.Write("[");
+                } else {
+                    Console.Write(" ");
+                }
+
+                if (stateContext.Tape[x]) {
+                    Console.Write("1");
+                } else {
+                    Console.Write("0");
+                }
+
+                if (x == stateContext.Position) {
+                    Console.Write("]");
+                } else {
+                    Console.Write(" ");
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+
+    public class StateContext {
+        public int Position { get; }
+        public bool[] Tape { get; }
+        public string NextState { get; }
+
+        public StateContext(int position, bool[] tape, string nextState) {
+            Position = position;
+            Tape = tape;
+            NextState = nextState;
+        }
+    }
+
+}
