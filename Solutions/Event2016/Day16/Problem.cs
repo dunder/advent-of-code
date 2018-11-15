a293dabd480c354d965ef710700045dca6831a7b
Matias Jansson <Matias.Jansson@datscha.com>
2018-11-15 13:02:15 +0100

    Day 16 2016 completed

---
+++ Solutions/Event2016/Day16/Problem.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace Solutions.Event2016.Day16
+{
+    public class Problem : ProblemBase
+    {
+        public override Event Event => Event.Event2016;
+        public override Day Day => Day.Day16;
+
+        private const string Input = "11100010111110100";
+
+        public override string FirstStar()
+        {
+            var checksum = CalculateChecksum(Input, diskSize: 272);
+            return checksum;
+        }
+
+        public override string SecondStar()
+        {
+            var checksum = CalculateChecksum(Input, diskSize: 35651584);
+            return checksum;
+        }
+
+        public static string Process(string input, int diskSize)
+        {
+            string output = Generate(input);
+            while (output.Length < diskSize)
+            {
+                output = Generate(output);
+            }
+
+            return output;
+        }
+        public static string Generate(string input)
+        {
+            var a = input;
+            var b = new string(input.Reverse().ToArray());
+            var temp = b.Replace('0', 'z');
+            temp = temp.Replace('1', '0');
+            temp = temp.Replace('z', '1');
+            return $"{a}0{temp}";
+        }
+
+        public static string Checksum(string input)
+        {
+            if (input.Length % 2 != 0) throw new ArgumentOutOfRangeException(nameof(input), "Must be even length");
+
+            var outputBuilder = new StringBuilder();
+            for (int i = 0; i < input.Length-1;i += 2)
+            {
+                outputBuilder.Append(input[i] == input[i + 1] ? "1" : "0");
+            }
+
+            return  outputBuilder.Length % 2 == 0 ? Checksum(outputBuilder.ToString()) : outputBuilder.ToString();
+        }
+
+        public static string CalculateChecksum(string input, int diskSize)
+        {
+            var diskContent = Process(input, diskSize);
+            var checksum = Checksum(diskContent.Substring(0, diskSize));
+            return checksum;
+        }
+    }
+}
---
+++ Solutions/Event2016/Day16/Tests.cs
@@ -0,0 +1,57 @@
+﻿using Xunit;
+
+namespace Solutions.Event2016.Day16
+{
+    public class Tests
+    {
+        [Theory]
+        [InlineData("1", "100")]
+        [InlineData("0", "001")]
+        [InlineData("11111", "11111000000")]
+        [InlineData("111100001010", "1111000010100101011110000")]
+        public void Generate(string input, string expected)
+        {
+            var generated = Problem.Generate(input);
+
+            Assert.Equal(expected, generated);
+        }
+
+        [Fact]
+        public void Process()
+        {
+            var output = Problem.Process("10000", 20);
+
+            Assert.Equal("10000011110010000111110", output);
+        }
+
+        [Fact]
+        public void Checksum()
+        {
+            var checksum = Problem.Checksum("110010110100");
+
+            Assert.Equal("100", checksum);
+        }
+
+        [Fact]
+        public void Problem1_Example()
+        {
+            var checksum = Problem.CalculateChecksum("10000", 20);
+
+            Assert.Equal("01100", checksum);
+        }
+
+        [Fact]
+        public void FirstStar()
+        {
+            var actual = new Problem().FirstStar();
+            Assert.Equal("10100011010101011", actual);
+        }
+
+        [Fact]
+        public void SecondStar()
+        {
+            var actual = new Problem().SecondStar();
+            Assert.Equal("01010001101011001", actual);
+        }
+    }
+}
