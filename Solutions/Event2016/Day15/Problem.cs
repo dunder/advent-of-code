cc380882de6c9f3677bc9718583e0696aa983911
Matias Jansson <Matias.Jansson@datscha.com>
2018-11-15 09:19:48 +0100

    Day 15 2016 completed

--- Solutions/Event2016/Day15/Problem.cs
+++ Solutions/Event2016/Day15/Problem.cs
@@ -1,4 +1,7 @@
-﻿namespace Solutions.Event2016.Day15
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Solutions.Event2016.Day15
 {
     public class Problem : ProblemBase
     {
@@ -7,16 +10,69 @@
 
         public override string FirstStar()
         {
-            var input = ReadInput();
-            var result = "Not implemented";
-            return result.ToString();
+            var discs = new List<Disc>
+            {
+                new Disc(1, 11, 13),
+                new Disc(2, 0, 5),
+                new Disc(3, 11, 17),
+                new Disc(4, 0, 3),
+                new Disc(5, 2, 7),
+                new Disc(6, 17, 19),
+
+            };
+
+            int tRelease = ReleaseAtToGetCapsule(discs);
+
+            return tRelease.ToString();
         }
 
         public override string SecondStar()
         {
-            var input = ReadInput();
-            var result = "Not implemented";
-            return result.ToString();
+            var discs = new List<Disc>
+            {
+                new Disc(1, 11, 13),
+                new Disc(2, 0, 5),
+                new Disc(3, 11, 17),
+                new Disc(4, 0, 3),
+                new Disc(5, 2, 7),
+                new Disc(6, 17, 19),
+                new Disc(7, 0, 11)
+            };
+
+            int tRelease = ReleaseAtToGetCapsule(discs);
+
+            return tRelease.ToString();
+        }
+
+        public class Disc
+        {
+            public int Offset { get; set; }
+            public int Position { get; set; }
+            public int Size { get; set; }
+
+            public Disc(int offset, int position, int size)
+            {
+                Offset = offset;
+                Position = position;
+                Size = size;
+            }
+
+            public int PositionAtPassage(int t)
+            {
+                return (Position + t + Offset) % Size;
+            }
+
+            public override string ToString()
+            {
+                return $"Position {Position}({Size})";
+            }
+        }
+
+        public static int ReleaseAtToGetCapsule(List<Disc> discs)
+        {
+            int t;
+            for (t = 0; discs.Any(d => d.PositionAtPassage(t) != 0); t++) {}
+            return t;
         }
     }
 }
--- Solutions/Event2016/Day15/Tests.cs
+++ Solutions/Event2016/Day15/Tests.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 using Xunit;
 
 namespace Solutions.Event2016.Day15
@@ -10,7 +8,51 @@ namespace Solutions.Event2016.Day15
         [Fact]
         public void FirstStarExample()
         {
+            var discs = new List<Problem.Disc>
+            {
+                new Problem.Disc(1, 4, 5),
+                new Problem.Disc(2, 1, 2)
+            };
 
+            int tRelease = Problem.ReleaseAtToGetCapsule(discs);
+
+            Assert.Equal(5, tRelease);
+        }
+
+        [Theory]
+        [InlineData(0,3,0,0)]
+        [InlineData(0,3,1,1)]
+        [InlineData(0,3,2,2)]
+        [InlineData(0,3,3,0)]
+        [InlineData(0,3,4,1)]
+        [InlineData(0,3,5,2)]
+        [InlineData(0,3,6,0)]
+        [InlineData(0,3,7,1)]
+        [InlineData(1,7,0,1)]
+        [InlineData(1,7,1,2)]
+        [InlineData(1,7,8,2)]
+        [InlineData(1,7,15,2)]
+        public void Disc_PositionAtPassage(int discInitialPosition, int discSize, int t, int expectedPosition)
+        {
+            var disc = new Problem.Disc(0, discInitialPosition, discSize);
+
+            int position = disc.PositionAtPassage(t);
+
+            Assert.Equal(expectedPosition, position);
+        }
+
+        [Theory]
+        [InlineData(0, 0)]
+        [InlineData(1, 1)]
+        [InlineData(2, 2)]
+        [InlineData(3, 0)]
+        public void Disc_PositionAtPassageWithOffset(int tPress, int expectedPosition)
+        {
+            var disc = new Problem.Disc(1, 2, 3);
+
+            int position = disc.PositionAtPassage(tPress);
+
+            Assert.Equal(expectedPosition, position);
         }
 
         [Fact]
