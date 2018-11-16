12233a3a444530fc1d5b5ce3e201cc361a433de9
Matias Jansson <Matias.Jansson@datscha.com>
2018-11-16 21:23:50 +0100

    Day 18 2016 completed

--- Solutions/Event2016/Day18/Problem.cs
+++ Solutions/Event2016/Day18/Problem.cs
@@ -1,4 +1,9 @@
-﻿namespace Solutions.Event2016.Day18
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Solutions.Event2016.Day18
 {
     public class Problem : ProblemBase
     {
@@ -10,14 +15,64 @@
 
         public override string FirstStar()
         {
-            var result = "Not implemented";
-            return result.ToString();
+            var safeTiles = CountSafeTiles(Input, 40);
+            return safeTiles.ToString();
         }
 
         public override string SecondStar()
         {
-            var result = "Not implemented";
-            return result.ToString();
+            var safeTiles = CountSafeTiles(Input, 400_000);
+            return safeTiles.ToString();
+        }
+
+        private static readonly HashSet<string> TrapCombos = new HashSet<string>
+        {
+            "^^.",
+            ".^^",
+            "^..",
+            "..^"
+        };
+        public static bool IsTrap(char left, char center, char right)
+        {
+            var combo = new String(new[] {left, center, right});
+
+            return TrapCombos.Contains(combo);
+        }
+
+        public static string NextRow(string row)
+        {
+            StringBuilder nextRow = new StringBuilder();
+            for (int i = 0; i < row.Length; i++)
+            {
+                var left = i == 0 ? '.' : row[i - 1];
+                var center = row[i];
+                var right = i == row.Length - 1 ? '.' : row[i + 1];
+
+                var nextRowForPosition = IsTrap(left, center, right) ? "^" : ".";
+
+                nextRow.Append(nextRowForPosition);
+            }
+            return nextRow.ToString();
+        }
+
+        public static List<string> CreateMap(string firstRow, int totalRows)
+        {
+            var map = new List<string> {firstRow};
+            var currentRow = firstRow;
+            foreach (var _ in Enumerable.Range(0, totalRows - 1))
+            {
+                currentRow = NextRow(currentRow);
+                map.Add(currentRow);
+            }
+
+            return map;
+        }
+
+        public static int CountSafeTiles(string firstRow, int totalRows)
+        {
+            var map = CreateMap(firstRow, totalRows);
+
+            return map.Aggregate(0, (a, row) => a + row.Count(t => t == '.'));
         }
     }
 }
--- Solutions/Event2016/Day18/Tests.cs
+++ Solutions/Event2016/Day18/Tests.cs
@@ -1,9 +1,99 @@
-﻿using Xunit;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Xunit;
 
 namespace Solutions.Event2016.Day18
 {
     public class Tests
     {
+        [Theory]
+        [InlineData('^','^','.')]
+        [InlineData('.','^','^')]
+        [InlineData('^','.','.')]
+        [InlineData('.','.','^')]
+        public void IsTrap(char left, char center, char right)
+        {
+            var isTrap = Problem.IsTrap(left, center, right);
+
+            Assert.True(isTrap);
+        }
+
+        [Theory]
+        [InlineData('.','^','.')]
+        [InlineData('.','.','.')]
+        [InlineData('^','.','^')]
+        [InlineData('^','^','^')]
+        public void IsNotTrap(char left, char center, char right)
+        {
+            var isTrap = Problem.IsTrap(left, center, right);
+
+            Assert.False(isTrap);
+        }
+
+        [Fact]
+        public void CreateMap()
+        {
+            var expectedMap = new List<string>
+            {
+                "..^^.",
+                ".^^^^",
+                "^^..^"
+            };
+
+            var map = Problem.CreateMap(expectedMap.First(), 3);
+
+            Assert.Equal(expectedMap, map);
+        }
+
+        [Fact]
+        public void CreateMap2()
+        {
+            var expectedMap = new List<string>
+            {
+                ".^^.^.^^^^",
+                "^^^...^..^",
+                "^.^^.^.^^.",
+                "..^^...^^^",
+                ".^^^^.^^.^",
+                "^^..^.^^..",
+                "^^^^..^^^.",
+                "^..^^^^.^^",
+                ".^^^..^.^^",
+                "^^.^^^..^^"
+            };
+
+            var map = Problem.CreateMap(expectedMap.First(), 10);
+
+            Assert.Equal(expectedMap, map);
+        }
+
+        [Fact]
+        public void FirstStar_Example()
+        {
+            var row = "..^^.";
+
+            var nextRow = Problem.NextRow(row);
+
+            Assert.Equal(".^^^^", nextRow);
+        }
+
+        [Fact]
+        public void FirstStar_Example2()
+        {
+            var row = ".^^^^";
+
+            var nextRow = Problem.NextRow(row);
+
+            Assert.Equal("^^..^", nextRow);
+        }
+
+        [Fact]
+        public void CountSafeTiles()
+        {
+            var safeTiles = Problem.CountSafeTiles(".^^.^.^^^^", 10);
+            Assert.Equal(38, safeTiles);
+        }
+
         [Fact]
         public void FirstStar()
         {
@@ -15,7 +105,7 @@ namespace Solutions.Event2016.Day18
         public void SecondStar()
         {
             var actual = new Problem().SecondStar();
-            Assert.Equal("20002936", actual);
+            Assert.Equal("20002936", actual); // 5.7 s
         }
     }
 }
