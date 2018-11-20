9f24b76abcefe46a23d1d2a6b2f26b7352a9858a
Matias Jansson <Matias.Jansson@datscha.com>
2018-11-20 07:47:23 +0100

    Work in progress day 20 2016

---
+++ Solutions/Event2016/Day20/Problem.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Solutions.Event2016.Day20
+{
+    public class Problem : ProblemBase
+    {
+        public override Event Event => Event.Event2016;
+        public override Day Day => Day.Day20;
+
+        public override string FirstStar()
+        {
+            var input = ReadLineInput();
+            var result = "Not implemented";
+            return result.ToString();
+        }
+
+        public override string SecondStar()
+        {
+            var input = ReadLineInput();
+            var result = "Not implemented";
+            return result.ToString();
+        }
+
+
+        public class IpRange
+        {
+            public int From { get; set; }
+            public int To { get; set; }
+
+            // this:  |----|          |----|   |----|    |--|
+            // other:     |----|   |----|       |--|    |----|
+            public bool Overlaps(IpRange other)
+            {
+                // this:    |----|  
+                // other: |----|     
+                var otherToWithin = other.To >= From && other.To <= To;
+
+                // this:  |----|    
+                // other:   |----|  
+                var otherFromWithin = other.From >= From && other.From <= To;
+
+                // this:  |----|
+                // other:  |--| 
+                var otherCovered = other.From >= From && other.To <= To;
+                
+                // this:   |--|
+                // other: |----|
+                var thisCovered = From >= other.From && To <= other.To;
+
+                return otherToWithin | otherFromWithin | otherCovered | thisCovered;
+            }
+
+            public IpRange Merge(IpRange other)
+            {
+                if (!Overlaps(other))
+                {
+                    throw new ArgumentOutOfRangeException(nameof(other), $"Cannot merge non overlapping ranges: {this} | {other}");
+                }
+
+                return new IpRange
+                {
+                    From = other.From < From ? other.From : From,
+                    To = other.To > To ? other.To : To
+                };
+            }
+
+            public override string ToString()
+            {
+                return $"{From}-{To}";
+            }
+        }
+
+        public static List<IpRange> ParseBlacklist(string[] blacklist)
+        {
+            var blackListRanges = new List<IpRange>();
+            foreach (var ipRange in blacklist)
+            {
+                var separated = ipRange.Split('-');
+                blackListRanges.Add(new IpRange
+                {
+                    From = int.Parse(separated[0]),
+                    To = int.Parse(separated[1])
+                });
+            }
+
+            return blackListRanges;
+        }
+
+        public class Blacklist
+        {
+            public int MaxValue { get; }
+
+            public Blacklist(int maxValue, List<IpRange> ipRangeBlacklist)
+            {
+                MaxValue = maxValue;
+
+                ipRangeBlacklist.Sort();
+
+                MergeOverlaps(ipRangeBlacklist);
+            }
+
+            private static void MergeOverlaps(List<IpRange> ipRangeBlacklist)
+            {
+                List<IpRange> mergedRanges = new List<IpRange>();
+                IpRange currentMerged = ipRangeBlacklist.First();
+                for (int i = 1; i < ipRangeBlacklist.Count; i++)
+                {
+                    IpRange current = ipRangeBlacklist[i];
+                    if (currentMerged.Overlaps(current))
+                    {
+                        currentMerged = currentMerged.Merge(current);
+                        current = currentMerged;
+                    }
+                    else
+                    {
+                        mergedRanges.Add(current);
+                    }
+                }
+            }
+
+            public int LowestValid()
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ Solutions/Event2016/Day20/Tests.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Solutions.Event2016.Day20
+{
+    public class Tests
+    {
+        [Fact]
+        public void BlacklistParser_CreatesIpRanges()
+        {
+            var blacklist = new[]
+            {
+                "5-8",
+                "0-2",
+                "4-7",
+            };
+
+            List<Problem.IpRange> blacklisted = Problem.ParseBlacklist(blacklist);
+
+
+            Assert.Equal(blacklist.Length, blacklisted.Count);
+
+            var sortedByFrom = blacklisted.OrderBy(b => b.From).ToList();
+
+            Assert.Equal(0, sortedByFrom[0].From);
+            Assert.Equal(2, sortedByFrom[0].To);
+            Assert.Equal(4, sortedByFrom[1].From);
+            Assert.Equal(7, sortedByFrom[1].To);
+            Assert.Equal(5, sortedByFrom[2].From);
+            Assert.Equal(8, sortedByFrom[2].To);
+        }
+
+        [Fact]
+        public void Blacklist()
+        {
+            var ipRangeBlacklist = new List<Problem.IpRange>
+            {
+                new Problem.IpRange(),
+            };
+
+            Problem.Blacklist blacklist = new Problem.Blacklist(9, ipRangeBlacklist);
+
+            Assert.Equal(3, blacklist.LowestValid());
+        }
+
+        [Theory]
+        [InlineData(0, 4, 0, 4, true)]
+        [InlineData(0, 4, 2, 5, true)]
+        [InlineData(0, 4, 1, 2, true)]
+        [InlineData(1, 2, 0, 4, true)]
+        [InlineData(0, 4, 5, 7, false)]
+        [InlineData(5, 7, 0, 4, false)]
+        public void IpRangeOverlaps(int r1From, int r1To, int r2From, int r2To, bool expectedOverlap)
+        {
+            var r1 = new Problem.IpRange { From = r1From, To = r1To };
+            var r2 = new Problem.IpRange { From = r2From, To = r2To };
+
+            var isOverlap = r1.Overlaps(r2);
+
+            Assert.Equal(expectedOverlap, isOverlap);
+        }
+
+        [Theory]
+        [InlineData(0,4,0,4,0,4)]
+        [InlineData(0,4,2,5,0,5)]
+        [InlineData(0,4,1,2,0,4)]
+        [InlineData(1,2,0,4,0,4)]
+        public void IpRangeMerge(int r1From, int r1To, int r2From, int r2To, int expectedFrom, int expectedTo)
+        {
+            var r1 = new Problem.IpRange {From = r1From, To = r1To};
+            var r2 = new Problem.IpRange {From = r2From, To = r2To};
+
+            var merged = r1.Merge(r2);
+
+            Assert.Equal(expectedFrom, merged.From);
+            Assert.Equal(expectedTo, merged.To);
+        }
+
+        [Fact]
+        public void FirstStar()
+        {
+            var actual = new Problem().FirstStar();
+            Assert.Equal("17348574", actual);
+        }
+
+        [Fact]
+        public void SecondStar()
+        {
+            var actual = new Problem().SecondStar();
+            Assert.Equal("104", actual);
+        }
+    }
+}
