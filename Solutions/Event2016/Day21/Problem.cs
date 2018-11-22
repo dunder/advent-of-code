65e888c605199112e08cb930c8bd2e8c3b990a87
Matias Jansson <Matias.Jansson@datscha.com>
2018-11-22 06:40:02 +0100

    Day 21 2016 done

---
+++ Solutions/Event2016/Day21/Problem.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Solutions.Event2016.Day21
+{
+    public class Problem : ProblemBase
+    {
+        public override Event Event => Event.Event2016;
+        public override Day Day => Day.Day21;
+
+        private const string Input1 = "abcdefgh";
+        private const string Input2 = "fbgdceah";
+
+        public override string FirstStar()
+        {
+            var instructions = ReadLineInput();
+            var result = Scramble(Input1, instructions);
+            return result;
+        }
+
+        public override string SecondStar()
+        {
+            var instructions = ReadLineInput();
+            var result = ReverseScramble(Input2, instructions.Reverse().ToList());
+            return result;
+        }
+
+        public static string Scramble(string input, IList<string> instructions)
+        {
+            var swapIndexExpression = new Regex(@"swap position (\d) with position (\d)");
+            var swapLetterExpression = new Regex(@"swap letter (\w) with letter (\w)");
+            var rotateRightExpression = new Regex(@"rotate right (\d) steps?");
+            var rotateLeftExpression = new Regex(@"rotate left (\d) steps?");
+            var rotateLetterExpression = new Regex(@"rotate based on position of letter (\w)");
+            var reverseExpression = new Regex(@"reverse positions (\d) through (\d)");
+            var moveExpression = new Regex(@"move position (\d) to position (\d)");
+
+            var scrambled = input;
+            foreach (var instruction in instructions)
+            {
+                switch (instruction)
+                {
+                    case var swapInstruction when swapIndexExpression.IsMatch(swapInstruction):
+                        var swapMatch = swapIndexExpression.Match(swapInstruction);
+                        var swapX = int.Parse(swapMatch.Groups[1].Value);
+                        var swapY = int.Parse(swapMatch.Groups[2].Value);
+                        scrambled = SwapIndex(scrambled, swapX, swapY);
+                        break;
+                    case var swapLetterInstruction when swapLetterExpression.IsMatch(swapLetterInstruction):
+                        var swapLetterMatch = swapLetterExpression.Match(swapLetterInstruction);
+                        var swapLetter1 = swapLetterMatch.Groups[1].Value.First();
+                        var swapLetter2 = swapLetterMatch.Groups[2].Value.First();
+                        scrambled = SwapLetter(scrambled, swapLetter1, swapLetter2);
+                        break;
+                    case var rotateRightInstruction when rotateRightExpression.IsMatch(rotateRightInstruction):
+                        var rotateRightMatch = rotateRightExpression.Match(rotateRightInstruction);
+                        var rightSteps = int.Parse(rotateRightMatch.Groups[1].Value);
+                        scrambled = RotateRight(scrambled, rightSteps);
+                        break;
+                    case var rotateLeftInstruction when rotateLeftExpression.IsMatch(rotateLeftInstruction):
+                        var rotateLeftMatch = rotateLeftExpression.Match(rotateLeftInstruction);
+                        var leftSteps = int.Parse(rotateLeftMatch.Groups[1].Value);
+                        scrambled = RotateLeft(scrambled, leftSteps);
+                        break;
+                    case var rotateLetterInstruction when rotateLetterExpression.IsMatch(rotateLetterInstruction):
+                        var rotateLetterMatch = rotateLetterExpression.Match(rotateLetterInstruction);
+                        var rotateLetter = rotateLetterMatch.Groups[1].Value.First();
+                        scrambled = RotateLetter(scrambled, rotateLetter);
+                        break;
+                    case var reverseInstruction when reverseExpression.IsMatch(reverseInstruction):
+                        var reverseMatch = reverseExpression.Match(reverseInstruction);
+                        var reverseIndex1 = int.Parse(reverseMatch.Groups[1].Value);
+                        var reverseIndex2 = int.Parse(reverseMatch .Groups[2].Value);
+                        scrambled = ReverseBetween(scrambled, reverseIndex1, reverseIndex2);
+                        break;
+                    case var moveInstruction when moveExpression.IsMatch(moveInstruction):
+                        var moveMatch = moveExpression.Match(moveInstruction);
+                        var moveX = int.Parse(moveMatch.Groups[1].Value);
+                        var moveY = int.Parse(moveMatch.Groups[2].Value);
+                        scrambled = Move(scrambled, moveX, moveY);
+                        break;
+                    default:
+                        throw new InvalidOperationException($"Cannot match instruction: {instruction}");
+                }
+            }
+            return scrambled;
+        }
+
+        public static string ReverseScramble(string input, IList<string> instructions)
+        {
+            var swapIndexExpression = new Regex(@"swap position (\d) with position (\d)");
+            var swapLetterExpression = new Regex(@"swap letter (\w) with letter (\w)");
+            var rotateRightExpression = new Regex(@"rotate right (\d) steps?");
+            var rotateLeftExpression = new Regex(@"rotate left (\d) steps?");
+            var rotateLetterExpression = new Regex(@"rotate based on position of letter (\w)");
+            var reverseExpression = new Regex(@"reverse positions (\d) through (\d)");
+            var moveExpression = new Regex(@"move position (\d) to position (\d)");
+
+            var scrambled = input;
+            foreach (var instruction in instructions)
+            {
+                switch (instruction)
+                {
+                    case var swapInstruction when swapIndexExpression.IsMatch(swapInstruction):
+                        var swapMatch = swapIndexExpression.Match(swapInstruction);
+                        var swapX = int.Parse(swapMatch.Groups[1].Value);
+                        var swapY = int.Parse(swapMatch.Groups[2].Value);
+                        scrambled = SwapIndex(scrambled, swapX, swapY);
+                        break;
+                    case var swapLetterInstruction when swapLetterExpression.IsMatch(swapLetterInstruction):
+                        var swapLetterMatch = swapLetterExpression.Match(swapLetterInstruction);
+                        var swapLetter1 = swapLetterMatch.Groups[1].Value.First();
+                        var swapLetter2 = swapLetterMatch.Groups[2].Value.First();
+                        scrambled = SwapLetter(scrambled, swapLetter1, swapLetter2);
+                        break;
+                    case var rotateRightInstruction when rotateRightExpression.IsMatch(rotateRightInstruction):
+                        var rotateRightMatch = rotateRightExpression.Match(rotateRightInstruction);
+                        var leftSteps = int.Parse(rotateRightMatch.Groups[1].Value);
+                        scrambled = RotateLeft(scrambled, leftSteps);
+                        break;
+                    case var rotateLeftInstruction when rotateLeftExpression.IsMatch(rotateLeftInstruction):
+                        var rotateLeftMatch = rotateLeftExpression.Match(rotateLeftInstruction);
+                        var rightSteps = int.Parse(rotateLeftMatch.Groups[1].Value);
+                        scrambled = RotateRight(scrambled, rightSteps);
+                        break;
+                    case var rotateLetterInstruction when rotateLetterExpression.IsMatch(rotateLetterInstruction):
+                        var rotateLetterMatch = rotateLetterExpression.Match(rotateLetterInstruction);
+                        var rotateLetter = rotateLetterMatch.Groups[1].Value.First();
+                        scrambled = RotateLetterReversed(scrambled, rotateLetter);
+                        break;
+                    case var reverseInstruction when reverseExpression.IsMatch(reverseInstruction):
+                        var reverseMatch = reverseExpression.Match(reverseInstruction);
+                        var reverseIndex1 = int.Parse(reverseMatch.Groups[1].Value);
+                        var reverseIndex2 = int.Parse(reverseMatch.Groups[2].Value);
+                        scrambled = ReverseBetween(scrambled, reverseIndex1, reverseIndex2);
+                        break;
+                    case var moveInstruction when moveExpression.IsMatch(moveInstruction):
+                        var moveMatch = moveExpression.Match(moveInstruction);
+                        var moveX = int.Parse(moveMatch.Groups[1].Value);
+                        var moveY = int.Parse(moveMatch.Groups[2].Value);
+                        scrambled = Move(scrambled, moveY, moveX);
+                        break;
+                    default:
+                        throw new InvalidOperationException($"Cannot match instruction: {instruction}");
+                }
+            }
+            return scrambled;
+        }
+
+
+        public static string SwapIndex(string input, int x, int y)
+        {
+            var outputBuilder = new StringBuilder(input)
+            {
+                [x] = input[y],
+                [y] = input[x]
+            };
+
+            return outputBuilder.ToString();
+        }
+
+        public static string SwapLetter(string input, char c1, char c2)
+        {
+            var outputBuilder = new StringBuilder(input);
+
+            outputBuilder.Replace(c1, '#');
+            outputBuilder.Replace(c2, c1);
+            outputBuilder.Replace('#', c2);
+
+            return outputBuilder.ToString();
+        }
+
+        public static string RotateLeft(string input, int steps)
+        {
+            return input.Substring(steps, input.Length - steps) + input.Substring(0, steps);
+        }
+
+        public static string RotateRight(string input, int steps)
+        {
+            steps = steps % input.Length;
+            return input.Substring(input.Length - steps, steps) + input.Substring(0, input.Length - steps);
+        }
+
+        public static string RotateLetter(string input, char letter)
+        {
+            int steps = 1;
+
+            steps += input.IndexOf(letter);
+
+            if (input.IndexOf(letter) >= 4)
+            {
+                steps++;
+            }
+            
+            return RotateRight(input, steps);
+        }
+
+        public static string RotateLetterReversed(string input, char letter)
+        {
+            var output = input;
+            do
+            {
+                output = RotateLeft(output, 1);
+            }
+            while (RotateLetter(output, letter) != input);
+
+            return output;
+        }
+
+        public static string ReverseBetween(string input, int x, int y)
+        {
+            var outputBuilder = new StringBuilder(input);
+
+            var reverse = input.Substring(x, y - x + 1);
+            var reversed = reverse.Reverse().ToList();
+            for (int i = x; i <= y; i++)
+            {
+                outputBuilder[i] = reversed[i - x];
+            }
+
+            return outputBuilder.ToString();
+        }
+
+        public static string Move(string input, int x, int y)
+        {
+            var outputBuilder = new StringBuilder(input);
+            var toMove = input[x];
+            outputBuilder.Remove(x, 1);
+            outputBuilder.Insert(y, toMove);
+
+            return outputBuilder.ToString();
+        }
+    }
+}
---
+++ Solutions/Event2016/Day21/Tests.cs
@@ -0,0 +1,161 @@
+﻿using Xunit;
+
+namespace Solutions.Event2016.Day21
+{
+    public class Tests
+    {
+        [Fact]
+        public void SwapIndex()
+        {
+            var input = "abcde";
+
+            var output = Problem.SwapIndex(input, 4, 0);
+
+            Assert.Equal("ebcda", output);
+        }
+
+        [Fact]
+        public void SwapIndexReversed()
+        {
+            var input = "ebcda";
+
+            var output = Problem.SwapIndex(input, 4, 0);
+
+            Assert.Equal("abcde", output);
+        }
+
+        [Fact]
+        public void SwapLetter()
+        {
+            var input = "ebcda";
+
+            var output = Problem.SwapLetter(input, 'd', 'b');
+
+            Assert.Equal("edcba", output);
+        }
+
+        [Fact]
+        public void SwapLetterReversed()
+        {
+            var input = "edcba";
+
+            var output = Problem.SwapLetter(input, 'd', 'b');
+
+            Assert.Equal("ebcda", output);
+        }
+
+        [Fact]
+        public void RotateLeft()
+        {
+            var input = "abcde";
+
+            var output = Problem.RotateLeft(input, 1);
+
+            Assert.Equal("bcdea", output);
+        }
+
+        [Fact]
+        public void RotateLeftReversed()
+        {
+            var input = "bcdea";
+
+            var output = Problem.RotateRight(input, 1);
+
+            Assert.Equal("abcde", output);
+        }
+
+        [Fact]
+        public void RotateRight()
+        {
+            var input = "abcde";
+
+            var output = Problem.RotateRight(input, 1);
+
+            Assert.Equal("eabcd", output);
+        }
+
+        [Fact]
+        public void RotateRightReversed()
+        {
+            var input = "eabcd";
+
+            var output = Problem.RotateLeft(input, 1);
+
+            Assert.Equal("abcde", output);
+        }
+
+        [Fact]
+        public void RotateLetter()
+        {
+            var input = "abdec";
+
+            var output = Problem.RotateLetter(input, 'b');
+
+            Assert.Equal("ecabd", output);
+        }
+
+        [Fact]
+        public void RotateLetterReversed()
+        {
+            var input = "ecabd";
+
+            var output = Problem.RotateLetterReversed(input, 'b');
+
+            Assert.Equal("abdec", output);
+        }
+
+        [Fact]
+        public void ReverseBetween()
+        {
+            var input = "abcde";
+
+            var output = Problem.ReverseBetween(input, 1, 3);
+
+            Assert.Equal("adcbe", output);
+        }
+
+        [Fact]
+        public void ReverseBetweenReversed()
+        {
+            var input = "adcbe";
+
+            var output = Problem.ReverseBetween(input, 1, 3);
+
+            Assert.Equal("abcde", output);
+        }
+
+        [Fact]
+        public void Move()
+        {
+            var input = "bdeac";
+
+            var output = Problem.Move(input, 3, 0);
+
+            Assert.Equal("abdec", output);
+        }
+
+        [Fact]
+        public void MoveReverse()
+        {
+            var input = "abdec";
+
+            var output = Problem.Move(input, 0, 3);
+
+            Assert.Equal("bdeac", output);
+        }
+
+        [Fact]
+        public void FirstStar()
+        {
+            var actual = new Problem().FirstStar();
+            Assert.Equal("gfdhebac", actual);
+        }
+
+        [Fact]
+        public void SecondStar()
+        {
+            var actual = new Problem().SecondStar();
+            Assert.Equal("dhaegfbc", actual);
+        }
+    }
+}
