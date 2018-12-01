01fd106604c5c218d15a286aba732c1e2c1332d5
Matias Jansson <Matias.Jansson@datscha.com>
2018-12-01 08:57:37 +0100

    Prepare for day 2 2018

---
+++ Solutions/Event2018/Day02/Problem.cs
@@ -0,0 +1,22 @@
+﻿namespace Solutions.Event2018.Day02
+{
+    public class Problem : ProblemBase
+    {
+        public override Event Event => Event.Event2018;
+        public override Day Day => Day.Day02;
+
+        public override string FirstStar()
+        {
+            var input = ReadInput();
+            var result = "Not implemented";
+            return result.ToString();
+        }
+
+        public override string SecondStar()
+        {
+            var input = ReadInput();
+            var result = "Not implemented";
+            return result.ToString();
+        }
+    }
+}
