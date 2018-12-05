f98006543382e27631898785cc26e91b0807533a
Matias Jansson <Matias.Jansson@datscha.com>
2018-12-05 06:46:17 +0100

    Started day 5 2018

--- Solutions/Event2018/Day05/Problem.cs
+++ Solutions/Event2018/Day05/Problem.cs
@@ -1,4 +1,6 @@
-﻿namespace Solutions.Event2018.Day05
+﻿using System.Text;
+
+namespace Solutions.Event2018.Day05
 {
     public class Problem : ProblemBase
     {
@@ -8,7 +10,7 @@
         public override string FirstStar()
         {
             var input = ReadInput();
-            var result = "Not implemented";
+            var result = ReduceAll(input);
             return result.ToString();
         }
 
@@ -16,6 +18,48 @@
         {
             var input = ReadInput();
             var result = "Not implemented";
+            return result.ToString();
+        }
+
+        public static int ReduceAll(string polymer)
+        {
+            string previous = "";
+            while (previous != polymer)
+            {
+                previous = polymer;
+                polymer = ReactReduce(polymer);
+            }
+
+            return polymer.Length;
+        }
+
+        public static string ReactReduce(string polymer)
+        {
+            var result = new StringBuilder();
+            for (int i = 0; i < polymer.Length - 1; i++)
+            {
+                var p1 = polymer[i];
+                var p2 = polymer[i + 1];
+
+                if (p1 == p2)
+                {
+                    result.Append(p1);
+                    continue;
+                };
+
+                if (char.ToLower(p1) == char.ToLower(p2))
+                {
+                    i++;
+                    continue;
+                }
+
+                result.Append(p1);
+                if (i == polymer.Length - 2)
+                {
+                    result.Append(p2);
+                }
+            }
+
             return result.ToString();
         }
     }
--- Solutions/Event2018/Day05/Tests.cs
+++ Solutions/Event2018/Day05/Tests.cs
@@ -4,11 +4,30 @@ namespace Solutions.Event2018.Day05
 {
     public class Tests
     {
+        [Theory]
+        [InlineData("dabAcCaCBAcCcaDA", "dabAaCBAcaDA")]
+        [InlineData("dabAaCBAcCcaDA", "dabCBAcaDA")]
+        public void ReactReduce(string polymer, string expected)
+        {
+            var reduced = Problem.ReactReduce(polymer);
+
+            Assert.Equal(expected, reduced);
+        }
+
+        [Fact]
+        public void FirstStarExample()
+        {
+            var polymer = "dabAcCaCBAcCcaDA";
+            var reduced = Problem.ReduceAll(polymer);
+
+            Assert.Equal(10, reduced);
+        }
+
         [Fact]
         public void FirstStar()
         {
             var actual = new Problem().FirstStar();
-            Assert.Equal("", actual);
+            Assert.Equal("", actual); // 10134 
         }
 
         [Fact]
