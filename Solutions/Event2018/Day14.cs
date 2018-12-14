439b8bb763460e48f8d787c2df2a1f020ade65da
Matias Jansson <Matias.Jansson@datscha.com>
2018-12-14 06:51:36 +0100

    Work on day 14 2018

--- Solutions/Event2018/Day14.cs
+++ Solutions/Event2018/Day14.cs
@@ -1,4 +1,8 @@
-﻿using Xunit;
+﻿using System;
+using System.Linq;
+using System.Reflection.Metadata;
+using Shared.Extensions;
+using Xunit;
 using static Solutions.InputReader;
 
 namespace Solutions.Event2018
@@ -8,6 +12,8 @@ namespace Solutions.Event2018
         public Event Event => Event.Event2018;
         public Day Day => Day.Day14;
         public string Name => "Day14";
+
+        public int Input => 793031;
 
         public string FirstStar()
         {
@@ -21,6 +27,63 @@ namespace Solutions.Event2018
             var input = ReadInput();
             var result = "Not implemented";
             return result.ToString();
+        }
+
+        public static long Score(int input, int next)
+        {
+            var scoreBoard = new[] {3, 7};
+
+
+            var elf1 = 0;
+            var elf2 = 1;
+
+
+            while (scoreBoard.Length < input + next)
+            {
+                var elf1Score = scoreBoard[elf1];
+                var elf2Score = scoreBoard[elf2];
+
+                var newScores = (elf1Score + elf2Score).ToString().Split().Select(int.Parse).ToArray();
+
+                scoreBoard = scoreBoard.Concat(newScores).ToArray();
+
+                elf1 = newScores.WrappedIndex(elf1Score + 1);
+                elf2 = newScores.WrappedIndex(elf2Score + 1);
+            }
+
+            return scoreBoard.Skip(input).Take(next).Sum();
+        }
+
+        public static int[] GenerateNewScoreBoard(int[] initialScoreBoard, int iterations)
+        {
+            var elf1 = 0;
+            var elf2 = 1;
+
+            var scoreBoard = initialScoreBoard;
+
+            for (int _ = 1; _ <= iterations; _++)
+            {
+                var elf1Score = scoreBoard[elf1];
+                var elf2Score = scoreBoard[elf2];
+
+                var newScores = (elf1Score + elf2Score).ToString().Split().Select(int.Parse).ToArray();
+
+                scoreBoard = scoreBoard.Concat(newScores).ToArray();
+
+                elf1 = newScores.WrappedIndex(elf1Score + 1);
+                elf2 = newScores.WrappedIndex(elf2Score + 1);
+            }
+
+            return scoreBoard;
+        }
+
+        [Fact]
+        public void FirstStartTest()
+        {
+            var result = Score(9, 10);
+
+            Assert.Equal("5158916779", result.ToString());
+            // 
         }
 
         [Fact]
