97b9ae868e6b5f46cc0e036b162320e59b768e02
Matias Jansson <Matias.Jansson@datscha.com>
2018-12-18 07:48:49 +0100

    Day 18 first star

---
+++ Solutions/Event2018/Day18.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using Shared.MapGeometry;
+using Xunit;
+using static Solutions.InputReader;
+
+namespace Solutions.Event2018
+{
+    public class Day18 : IDay
+    {
+        public Event Event => Event.Event2018;
+        public Day Day => Day.Day18;
+        public string Name => "Settlers of The North Pole";
+
+        public string FirstStar()
+        {
+            var input = ReadLineInput();
+            var result = TotalValueAfter(input, 10);
+            return result.ToString();
+        }
+
+        private long TotalValueAfter(IList<string> input, int count)
+        {
+            var land = Land.Parse(input);
+
+            for (int i = 0; i < count; i++)
+            {
+                land = land.Next();
+            }
+
+            return land.Trees.Count * land.Lumberyard.Count;
+        }
+
+        public string SecondStar()
+        {
+            var input = ReadInput();
+            var result = "Not implemented";
+            return result.ToString();
+        }
+
+        public class Land
+        {
+            private readonly int width;
+            private readonly int height;
+
+            public HashSet<Point> Lumberyard { get; }
+            public HashSet<Point> Trees { get; }
+
+            private Land(HashSet<Point> lumberYard, HashSet<Point> trees, int width, int height)
+            {
+                Lumberyard = lumberYard;
+                Trees = trees;
+                this.width = width;
+                this.height = height;
+            }
+
+            public static Land Parse(IList<string> input)
+            {
+                var land = new Land(new HashSet<Point>(), new HashSet<Point>(), input.First().Length, input.Count);
+
+                for (int y = 0; y < land.height; y++)
+                {
+                    var line = input[y];
+                    for (int x = 0; x < land.width; x++)
+                    {
+                        var point = new Point(x,y);
+                        var acre = line[x];
+                        if (acre == '|')
+                        {
+                            land.Trees.Add(point);
+                        }
+                        else if (acre == '#')
+                        {
+                            land.Lumberyard.Add(point);
+                        }
+
+                    }
+                }
+
+                return land;
+            }
+
+            public Land Next()
+            {
+                var newLumberyard = new HashSet<Point>();
+                var newTrees = new HashSet<Point>();
+
+                for (int y = 0; y < height; y++)
+                {
+                    for (int x = 0; x < width; x++)
+                    {
+                        var point = new Point(x,y);
+                        if (Trees.Contains(point))
+                        {
+                            ConvertTree(point, newLumberyard, newTrees);
+                        }
+                        else if (Lumberyard.Contains(point))
+                        {
+                            ConvertLumberyard(point, newLumberyard, newTrees);
+                        }
+                        else
+                        {
+                            ConvertOpen(point, newLumberyard, newTrees);
+                        }
+                    }
+                }
+
+                return new Land(newLumberyard, newTrees, width, height);
+            }
+
+            public override string ToString()
+            {
+                var landString = new StringBuilder();
+                for (int y = 0; y < height; y++)
+                {
+                    for (int x = 0; x < width; x++)
+                    {
+                        var point = new Point(x, y);
+                        if (Trees.Contains(point))
+                        {
+                            landString.Append('|');
+                        }
+                        else if (Lumberyard.Contains(point))
+                        {
+                            landString.Append('#');
+                        }
+                        else
+                        {
+                            landString.Append('.');
+                        }
+                    }
+
+                    landString.AppendLine();
+                }
+
+                return landString.ToString();
+            }
+
+            private void ConvertOpen(Point point, HashSet<Point> newLumberyard, HashSet<Point> newTrees)
+            {
+                var adjacent = point.AdjacentInAllDirections()
+                    .Where(p => p.X >= 0 && p.X < width && p.Y >= 0 && p.Y < height);
+
+                if (adjacent.Count(a => Trees.Contains(a)) >= 3)
+                {
+                    newTrees.Add(point);
+                }
+            }
+
+            private void ConvertLumberyard(Point point, HashSet<Point> newLumberyard, HashSet<Point> newTrees)
+            {
+                var adjacent = point.AdjacentInAllDirections()
+                    .Where(p => p.X >= 0 && p.X < width && p.Y >= 0 && p.Y < height)
+                    .ToList();
+
+                if (adjacent.Count(a => Trees.Contains(a)) > 0 && adjacent.Count(a => Lumberyard.Contains(a)) > 0)
+                {
+                    newLumberyard.Add(point);
+                }
+            }
+
+            private void ConvertTree(Point point, HashSet<Point> newLumberyard, HashSet<Point> newTrees)
+            {
+                var adjacent = point.AdjacentInAllDirections()
+                    .Where(p => p.X >= 0 && p.X < width && p.Y >= 0 && p.Y < height)
+                    .ToList();
+
+                if (adjacent.Count(a => Lumberyard.Contains(a)) >= 3)
+                {
+                    newLumberyard.Add(point);
+                }
+                else
+                {
+                    newTrees.Add(point);
+                }
+            }
+        }
+
+        [Fact]
+        public void Next()
+        {
+            var lines = new[]
+            {
+                ".#.#...|#.",
+                ".....#|##|",
+                ".|..|...#.",
+                "..|#.....#",
+                "#.#|||#|#|",
+                "...#.||...",
+                ".|....|...",
+                "||...#|.#|",
+                "|.||||..|.",
+                "...#.|..|."
+            };
+            var expectedLines = new[]
+            {
+                ".......##.",
+                "......|###",
+                ".|..|...#.",
+                "..|#||...#",
+                "..##||.|#|",
+                "...#||||..",
+                "||...|||..",
+                "|||||.||.|",
+                "||||||||||",
+                "....||..|."
+            };
+
+            var land = Land.Parse(lines);
+            var expectedLand = Land.Parse(expectedLines);
+
+            var newLand = land.Next();
+
+            Assert.Equal(expectedLand.ToString(), newLand.ToString());
+        }
+
+        [Fact]
+        public void FirstStarExample()
+        {
+            var lines = new[]
+            {
+                ".#.#...|#.",
+                ".....#|##|",
+                ".|..|...#.",
+                "..|#.....#",
+                "#.#|||#|#|",
+                "...#.||...",
+                ".|....|...",
+                "||...#|.#|",
+                "|.||||..|.",
+                "...#.|..|."
+            };
+
+            var value = TotalValueAfter(lines, 10);
+
+            Assert.Equal(1147, value);
+        }
+
+        [Fact]
+        public void FirstStarTest()
+        {
+            var actual = FirstStar();
+            Assert.Equal("", actual);
+        }
+
+        [Fact]
+        public void SecondStarTest()
+        {
+            var actual = SecondStar();
+            Assert.Equal("", actual);
+        }
+    }
+}
