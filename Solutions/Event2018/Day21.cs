86c706eeffbc46263b1a73605e6aa3d1abfe14db
Matias Jansson <Matias.Jansson@datscha.com>
2018-12-28 11:13:33 +0100

    Day 21 2018 done

--- Solutions/Event2018/Day21.cs
+++ Solutions/Event2018/Day21.cs
@@ -15,29 +15,25 @@ namespace Solutions.Event2018
         public string FirstStar()
         {
             var input = ReadLineInput();
-            var result = RunProgram(input, 0, 0, new []{ 6483199, 0,0,0,0,0});
+            var result = RunProgram1(input, new[] { 0, 0, 0, 0, 0, 0 });
             return result.ToString();
         }
 
         public string FirstStarConsole()
         {
-            // i = 18
-            // x, 9784884, 65536, 0, 17, 255 (counter = 23 + 7*(255-1) = 1801)
-            // x, 9784884, 6548735, 0, 17, 25579
-            // x, 1508639, 15202945, 0, 17, 59385
             var input = ReadLineInput();
-            var result = RunProgram(input, 18, 1801, new[] { 0, 9784884, 65536, 0, 17, 255 }, true);
+            var result = RunProgram2(input, new[] { 0, 0, 0, 0, 0, 0 }, true);
             return result.ToString();
         }
 
         public string SecondStar()
         {
-            var input = ReadInput();
-            var result = "Not implemented";
+            var input = ReadLineInput();
+            var result = RunProgram2(input, new[] { 0, 0, 0, 0, 0, 0 });
             return result.ToString();
         }
 
-        public int RunProgram(IList<string> program, int i, int instructionCounter, int[] registry, bool print = false)
+        public int RunProgram1(IList<string> program, int[] registry, bool print = false)
         {
             var instructionPointerRegistry = int.Parse(program[0].Substring(4, 1));
 
@@ -48,13 +44,13 @@ namespace Solutions.Event2018
                 Print(programInstructions);
             }
 
-            for (; i < programInstructions.Count; i++)
+            for (var i = 0; i < programInstructions.Count; i++)
             {
                 var programInstruction = programInstructions[(int) i];
 
                 if (print)
                 {
-                    Print(i, programInstruction, registry, instructionCounter);
+                    Print(i, programInstruction, registry);
                 }
 
                 var op = programInstruction.Substring(0, 4);
@@ -70,14 +66,89 @@ namespace Solutions.Event2018
 
                 registry[instructionPointerRegistry] = i;
 
+                if (programInstruction == "eqrr 1 0 5")
+                {
+                    return registry[1];
+                }
+
                 registry = Instructions[op](registry, 0, a, b, c);
 
                 i = registry[instructionPointerRegistry];
-
-                instructionCounter++;
             }
 
-            return instructionCounter;
+            return -1;
+        }
+
+        public int RunProgram2(IList<string> program, int[] registry, bool print = false)
+        {
+            var instructionPointerRegistry = int.Parse(program[0].Substring(4, 1));
+
+            var programInstructions = program.Skip(1).ToList();
+
+            if (print)
+            {
+                Print(programInstructions);
+            }
+
+            var valuesOfR1 = new HashSet<int>();
+            var repeatingSequence = new List<int>();
+            var repeatingSequenceValues = new HashSet<int>();
+
+            for (var i = 0; i < programInstructions.Count; i++)
+            {
+                var programInstruction = programInstructions[(int)i];
+
+                if (print)
+                {
+                    Print(i, programInstruction, registry);
+                }
+
+                var op = programInstruction.Substring(0, 4);
+
+                var arguments = programInstruction.Substring(5)
+                    .Split(" ")
+                    .Select(int.Parse)
+                    .ToArray();
+
+                var a = arguments[0];
+                var b = arguments[1];
+                var c = arguments[2];
+
+                registry[instructionPointerRegistry] = i;
+                if (programInstruction == "addi 5 1 5")
+                {
+                    var r2 = registry[2];
+                    var x = r2 / 256;
+                    while ((x + 1) * 256 <= r2) { x++; }
+
+                    registry[5] = x;
+                }
+                else
+                {
+                    if (programInstruction == "eqrr 1 0 5")
+                    { 
+                        var r1 = registry[1];
+                        if (valuesOfR1.Contains(r1))
+                        {
+                            if (repeatingSequenceValues.Contains(r1))
+                            {
+                                return repeatingSequence.Last();
+                            }
+
+                            repeatingSequence.Add(r1);
+                            repeatingSequenceValues.Add(r1);
+                        }
+
+                        valuesOfR1.Add(r1);
+                    }
+
+                    registry = Instructions[op](registry, 0, a, b, c);
+                }
+
+                i = registry[instructionPointerRegistry];
+            }
+
+            return -1;
         }
 
         public void Print(IList<string> instructions)
@@ -91,7 +162,7 @@ namespace Solutions.Event2018
             }
         }
 
-        public void Print(long i, string instruction, int[] registry, int instructionCount)
+        public void Print(long i, string instruction, int[] registry)
         {
             var registry0 = registry[0].ToString();
             var registry1 = registry[1].ToString();
@@ -114,7 +185,7 @@ namespace Solutions.Event2018
             Console.CursorLeft = 0;
 
             Console.WriteLine();
-            Console.WriteLine($"i: {i.ToString().PadLeft(2, ' ')} ({instructionCount.ToString().PadLeft(14, ' ')})");
+            Console.WriteLine($"i: {i.ToString().PadLeft(2, ' ')}");
             Console.WriteLine();
             Console.WriteLine($"0: {registry0.PadLeft(20, ' ')}");
             Console.WriteLine($"1: {registry1.PadLeft(20, ' ')}");
@@ -130,19 +201,19 @@ namespace Solutions.Event2018
         public void FirstStarTest()
         {
             var actual = FirstStar();
-            Assert.Equal("", actual); // register 0 input = 6483199
+            Assert.Equal("6483199", actual);
         }
 
         [Fact]
         public void SecondStarTest()
         {
             var actual = SecondStar();
-            Assert.Equal("", actual); // register 0 input = ?
+            Assert.Equal("13338900", actual);
         }
 
         private static readonly Dictionary<string, Func<int[], int, int, int, int, int[]>> Instructions =
-    new Dictionary<string, Func<int[], int, int, int, int, int[]>>
-{
+            new Dictionary<string, Func<int[], int, int, int, int, int[]>>
+        {
             {"addr", (rin, opCode, a, b, c) =>
             {
                 var rout = rin.ToList();
@@ -271,7 +342,6 @@ namespace Solutions.Event2018
 
                 return rout.ToArray();
             }}
-};
-
+        };
     }
 }
