b635ece9334075876e3fe164ef2eb7cba7b4e24b
Matias Jansson <matiasjansson@gmail.com>
2019-11-23 17:04:45 +0100

    All prepared for solving problem

---
+++ Solutions/Event2015/Day05.cs
@@ -0,0 +1,41 @@
+﻿using Xunit;
+using static Solutions.InputReader;
+
+namespace Solutions.Event2015
+{
+    // --- Day 5: Doesn't He Have Intern-Elves For This? ---
+    public class Day05
+    {
+
+        public static int FirstStar()
+        {
+            var input = ReadLineInput();
+
+            return 0;
+        }
+
+        public static int SecondStar()
+        {
+            var input = ReadLineInput();
+
+            return 0;
+        }
+
+        [Fact]
+        public void FirstStarTest()
+        {
+            var result = FirstStar();
+
+            Assert.Equal(238, result);
+        }
+
+        [Fact]
+        public void SecondStarTest()
+        {
+            var result = SecondStar();
+
+            Assert.Equal(69, result);
+        }
+
+    }
+}
