ff6b6661a6912b7f441361ffa9aee34852dfbb64
Matias Jansson <matiasjansson@gmail.com>
2019-11-24 13:25:57 +0100

    Solved 2015 Day 5

--- Solutions/Event2015/Day05.cs
+++ Solutions/Event2015/Day05.cs
@@ -27,9 +27,34 @@ namespace Solutions.Event2015
             return forbidden.Any(input.Contains);
         }
 
+        public static bool HasPairOfTwoTwice(string input)
+        {
+            for (int i = 0; i < input.Length - 2; i++)
+            {
+                var pair = new string(new[] {input[i], input[i + 1]});
+                if (input.Substring(i + 2).Contains(pair))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public static bool HasLetterRepeatedWithOneBetween(string input)
+        {
+            var pattern = @"([a-z])[a-z]\1";
+            return Regex.IsMatch(input, pattern);
+        }
+
         public static bool IsNice(string input)
         {
             return HasThreeVowels(input) && HasOneLetterTwiceInARow(input) && !HasAnyForbidden(input);
+        }
+
+        public static bool IsNice2(string input)
+        {
+            return HasPairOfTwoTwice(input) && HasLetterRepeatedWithOneBetween(input);
         }
 
         public static int FirstStar()
@@ -43,7 +68,7 @@ namespace Solutions.Event2015
         {
             var input = ReadLineInput();
 
-            return 0;
+            return input.Count(IsNice2);
         }
 
         [Fact]
@@ -101,6 +126,40 @@ namespace Solutions.Event2015
         public void FirstStar_Examples(string input, bool expectedNice)
         {
             var isNice = IsNice(input);
+
+            Assert.Equal(expectedNice, isNice);
+        }
+
+        [Theory]
+        [InlineData("xyxy", true)]
+        [InlineData("aabcdefgaa", true)]
+        [InlineData("aaa", false)]
+        public void TwoLettersTwice_Examples(string input, bool expected)
+        {
+            var actual = HasPairOfTwoTwice(input);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("xyx")]
+        [InlineData("abcdefeghi")]
+        [InlineData("aaa")]
+        public void HasLetterRepeatedWithOneBetween_Examples(string input)
+        {
+            var found = HasLetterRepeatedWithOneBetween(input);
+
+            Assert.True(found);
+        }
+
+        [Theory]
+        [InlineData("qjhvhtzxzqqjkmpb", true)]
+        [InlineData("xxyxx", true)]
+        [InlineData("uurcxstgmygtbstg", false)]
+        [InlineData("ieodomkazucvgmuy", false)]
+        public void SecondStar_Examples(string input, bool expectedNice)
+        {
+            var isNice = IsNice2(input);
 
             Assert.Equal(expectedNice, isNice);
         }
