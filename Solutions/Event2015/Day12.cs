3baa8b68e831443e5e11afc68c3580ca0038ca49
Matias Jansson <matiasjansson@gmail.com>
2019-12-01 16:10:07 +0100

    Prepare 2015 day 12

---
+++ Solutions/Event2015/Day12.cs
@@ -0,0 +1,38 @@
+﻿using Xunit;
+using static Solutions.InputReader;
+
+namespace Solutions.Event2015
+{
+    // --- Day 12: JSAbacusFramework.io ---
+    public class Day12
+    {
+        public static int FirstStar()
+        {
+            var input = ReadInput();
+            return 0;
+        }
+
+        public static int SecondStar()
+        {
+            var input = ReadInput();
+            return 0;
+        }
+
+        [Fact]
+        public void FirstStarTest()
+        {
+            var result = FirstStar();
+
+            Assert.Equal(-1, result);
+        }
+
+        [Fact]
+        public void SecondStarTest()
+        {
+            var result = SecondStar();
+
+            Assert.Equal(-1, result);
+        }
+
+    }
+}
