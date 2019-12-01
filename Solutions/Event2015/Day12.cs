355bbedb1b8902cf5b9f4b8ec719c4e50cefd6f7
Matias Jansson <matiasjansson@gmail.com>
2019-12-01 16:51:41 +0100

    Solved 2015 day 12 star 1

--- Solutions/Event2015/Day12.cs
+++ Solutions/Event2015/Day12.cs
@@ -1,4 +1,6 @@
-﻿using Xunit;
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+using Xunit;
 using static Solutions.InputReader;
 
 namespace Solutions.Event2015
@@ -6,10 +8,15 @@ namespace Solutions.Event2015
     // --- Day 12: JSAbacusFramework.io ---
     public class Day12
     {
+        public static int SumNumbers(string input)
+        {
+            var expression = new Regex(@"-?\d+");
+            return expression.Matches(input).Sum(m => int.Parse(m.Value));
+        }
         public static int FirstStar()
         {
             var input = ReadInput();
-            return 0;
+            return SumNumbers(input);
         }
 
         public static int SecondStar()
@@ -32,6 +39,21 @@ namespace Solutions.Event2015
             var result = SecondStar();
 
             Assert.Equal(-1, result);
+        }
+
+        [Theory]
+        [InlineData(@"[1,2,3]", 6)]
+        [InlineData(@"{""a"":2,""b"":4}", 6)]
+        [InlineData(@"[[[3]]]", 3)]
+        [InlineData(@"{""a"":{""b"":4},""c"":-1}", 3)]
+        [InlineData(@"{""a"":[-1,1]}", 0)]
+        [InlineData(@"[-1,{""a"":1}]", 0)]
+        [InlineData(@"[]", 0)]
+        [InlineData(@"{}", 0)]
+        public void FirstStarExamples(string input, int expectedSum)
+        {
+            var sum = SumNumbers(input);
+            Assert.Equal(expectedSum, sum);
         }
 
     }
