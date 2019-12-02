1d22986daec725b75b43ae022781fb149b00abd7
Matias Jansson <matiasjansson@gmail.com>
2019-12-02 08:34:47 +0100

    Prepare 2019 day 3

---
+++ Solutions/Event2019/Day03.cs
@@ -0,0 +1,34 @@
+﻿using Xunit;
+using static Solutions.InputReader;
+
+
+namespace Solutions.Event2019
+{
+    // 
+    public class Day03
+    {
+        public int FirstStar()
+        {
+            var input = ReadLineInput();
+            return 0;
+        }
+
+        public int SecondStar()
+        {
+            var input = ReadLineInput();
+            return 0;
+        }
+
+        [Fact]
+        public void FirstStarTest()
+        {
+            Assert.Equal(-1, FirstStar());
+        }
+
+        [Fact]
+        public void SecondStarTest()
+        {
+            Assert.Equal(-1, SecondStar());
+        }
+    }
+}
