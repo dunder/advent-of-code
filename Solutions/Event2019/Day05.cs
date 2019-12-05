64f0487d1e3c73d9dab6729990dd3e8311c1955a
Matias Jansson <matiasjansson@gmail.com>
2019-12-05 19:00:20 +0100

    Solved first star 2019 day 5 (again)

--- Solutions/Event2019/Day05.cs
+++ Solutions/Event2019/Day05.cs
@@ -15,8 +15,16 @@ namespace Solutions.Event2019
             return input.Split(',').Select(int.Parse).ToList();
         }
 
-        public static void Execute(List<int> code)
+        private enum Mode
         {
+            Position = 0,
+            Immediate
+        }
+
+        public static int Execute(List<int> code, int input)
+        {
+            var output = new List<int>();
+
             for (int i = 0; i < code.Count;)
             {
                 // 0 - position mode
@@ -26,62 +34,64 @@ namespace Solutions.Event2019
                 var op = instruction % 100;
                 var p1 = (instruction / 100) % 10;
                 var p2 = (instruction / 1000) % 10;
-                var p3 = (instruction / 1000) % 10;
 
                 switch (op)
                 {
                     case 1:
-                    {
-                        var arg1 = code[i + 1];
-                        var arg2 = code[i + 2];
-                        var writeTo = code[i + 3];
-                        arg1 = p1 == 0 ? code[arg1] : arg1;
-                        arg2 = p1 == 0 ? code[arg2] : arg2;
-                        var result = arg1 + arg2;
-                        code[writeTo] = result;
-                        i = i + 4;
-                    }
+                        {
+                            var arg1 = code[i + 1];
+                            var arg2 = code[i + 2];
+                            var writeTo = code[i + 3];
+                            arg1 = p1 == 0 ? code[arg1] : arg1;
+                            arg2 = p2 == 0 ? code[arg2] : arg2;
+                            var result = arg1 + arg2;
+                            code[writeTo] = result;
+                            i = i + 4;
+                        }
                         break;
                     case 2:
-                    {
-                        var arg1 = code[i + 1];
-                        var arg2 = code[i + 2];
-                        var writeTo = code[i + 3];
-                        arg1 = p1 == 0 ? code[arg1] : arg1;
-                        arg2 = p2 == 0 ? code[arg2] : arg2;
-                        var result = arg1 * arg2;
-                        code[writeTo] = result;
-                        i = i + 4;
-                    }
+                        {
+                            var arg1 = code[i + 1];
+                            var arg2 = code[i + 2];
+                            var writeTo = code[i + 3];
+                            arg1 = p1 == 0 ? code[arg1] : arg1;
+                            arg2 = p2 == 0 ? code[arg2] : arg2;
+                            var result = arg1 * arg2;
+                            code[writeTo] = result;
+                            i = i + 4;
+                        }
                         break;
                     case 3:
-                    {
-                        var arg1 = code[i + 1];
-                        code[arg1] = 0;
-                        i = i + 2;
-                    }
+                        {
+                            var arg1 = code[i + 1];
+                            code[arg1] = input;
+                            i = i + 2;
+                        }
                         break;
                     case 4:
-                    {
-                        var arg1 = code[i + 1];
-                        code[arg1] = 0;
-                        i = i + 2;
-                    }
+                        {
+                            var arg1 = code[i + 1];
+                            arg1 = p1 == 0 ? code[arg1] : arg1;
+                            output.Add(arg1);
+                            i = i + 2;
+                        }
                         break;
                     case 99:
-                    {
-                        return;
-                    }
+                        {
+                            return output.Last();
+                        }
                     default:
                         throw new InvalidOperationException($"Unknown op '{op}' code at i = {i}");
                 }
             }
+            throw new InvalidOperationException("Unexpected program exit");
         }
 
         public int FirstStar()
         {
-            var input = ReadLineInput();
-            return 0;
+            var input = ReadInput();
+            var code = Parse(input);
+            return Execute(code, 1);
         }
 
         public int SecondStar()
@@ -93,13 +103,13 @@ namespace Solutions.Event2019
         [Fact]
         public void FirstStarTest()
         {
-            Assert.Equal(-1, FirstStar());
+            Assert.Equal(7265618, FirstStar());
         }
 
         [Fact]
         public void SecondStarTest()
         {
-            Assert.Equal(-1, SecondStar());
+            Assert.Equal(7731427, SecondStar());
         }
 
         [Fact]
