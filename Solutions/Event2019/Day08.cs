52324b1bf9729ba2fcea7cc22c8cf70cc98f27f5
Matias Jansson <matiasjansson@gmail.com>
2019-12-08 06:09:51 +0100

    Solved star 1 2019 day 8

--- Solutions/Event2019/Day08.cs
+++ Solutions/Event2019/Day08.cs
@@ -1,4 +1,7 @@
-﻿using Xunit;
+﻿using System.Collections.Generic;
+using System.Linq;
+using MoreLinq;
+using Xunit;
 using static Solutions.InputReader;
 
 
@@ -7,11 +10,23 @@ namespace Solutions.Event2019
     // 
     public class Day08
     {
-        
-        public int FirstStar()
+        public void Parse(IEnumerable<string> input)
         {
-            var input = ReadLineInput();
-            return 0;
+
+        }
+
+        public static int CountZeroes(string input)
+        {
+            int width = 25;
+            int height = 6;
+
+            var fewestZeroes = input.Batch(width * height).OrderBy(layer => layer.Count(c => c == '0')).First().ToList();
+            return fewestZeroes.Count(c => c == '1') * fewestZeroes.Count(c => c == '2');
+        }
+        public int FirstStar()  
+        {
+            var input = ReadInput();
+            return CountZeroes(input);
         }
 
         public int SecondStar()
