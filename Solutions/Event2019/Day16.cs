b53bf39f6b2a1c93e031008b4dbe04ababa9bb44
Matias Jansson <matiasjansson@gmail.com>
2019-12-16 10:49:30 +0100

    Prepared 2019 day 16

---
+++ Solutions/Event2019/Day16.cs
@@ -0,0 +1,34 @@
+﻿using Xunit;
+using static Solutions.InputReader;
+
+
+namespace Solutions.Event2019
+{
+    // --- Day 16: Flawed Frequency Transmission ---
+    public class Day16
+    {
+        public int FirstStar()
+        {
+            var input = ReadInput();
+            return 0;
+        }
+
+        public int SecondStar()
+        {
+            var input = ReadInput();
+            return 0;
+        }
+
+        [Fact]
+        public void FirstStarTest()
+        {
+            Assert.Equal(-1, FirstStar());
+        }
+
+        [Fact]
+        public void SecondStarTest()
+        {
+            Assert.Equal(-1, SecondStar());
+        }
+    }
+}
