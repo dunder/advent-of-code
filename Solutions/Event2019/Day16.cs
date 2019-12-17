9b05ed5f77a336b01f04f747777e10bb95791a76
Matias Jansson <matiasjansson@gmail.com>
2019-12-17 08:22:12 +0100

    Solved 2019 day 16 star 1

--- Solutions/Event2019/Day16.cs
+++ Solutions/Event2019/Day16.cs
@@ -23,9 +23,15 @@ namespace Solutions.Event2019
             for (int phase = 1; phase <= phases; phase++)
             {
                 var output = new List<int>();
-                for (int outputPosition = 0; outputPosition < input.Count; outputPosition++)
+                for (int outputPosition = 1; outputPosition <= input.Count; outputPosition++)
                 {
-                    var positionPattern = pattern.SelectMany(d => Enumerable.Repeat(d, outputPosition + 1)).Skip(1).ToList();
+                    var positionPattern = pattern
+                        .SelectMany(d => Enumerable.Repeat(d, outputPosition))
+                        .ToList();
+                    var first = positionPattern[0];
+                    positionPattern = positionPattern.Skip(1).ToList();
+                    positionPattern.Add(first);
+
                     var positionOutput = new List<int>();
                     for (int digit = 0; digit < input.Count; digit++)
                     {
@@ -33,13 +39,15 @@ namespace Solutions.Event2019
                         var patternValue = positionPattern[patternIndex];
                         var outputValue = input[digit];
                         var positionValue = outputValue * patternValue;
-                        if (positionValue > 10)
+                        if (positionValue > 9)
                         {
                             positionValue = positionValue % 10;
                         }
                         positionOutput.Add(positionValue);
                     }
-                    output.Add(Math.Abs(positionOutput.Sum()));
+
+                    var outputForPosition = Math.Abs(positionOutput.Sum());
+                    output.Add(outputForPosition > 9 ? outputForPosition % 10 : outputForPosition);
                 }
 
                 input = output;
