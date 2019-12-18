4ebd1d5c9af7ce30eac9d94b62c04aee18c02614
Matias Jansson <matiasjansson@gmail.com>
2019-12-18 05:51:01 +0100

    Prepared 2019 day 18

--- Solutions/Event2019/Day16.cs
+++ Solutions/Event2019/Day16.cs
@@ -61,6 +61,45 @@ namespace Solutions.Event2019
             return s.ToString();
         }
 
+        private static string Run2(List<int> input, int phases)
+        {
+            var offset = int.Parse(string.Join("", input.Take(7).Select(c => int.Parse(c.ToString()))));
+            
+            for (int phase = 1; phase <= phases; phase++)
+            {
+                var output = new List<int>();
+                for (int outputPosition = 1; outputPosition <= input.Count; outputPosition++)
+                {
+                    // mönstret måste upprepa sig med multipel mellan längd för input och output position * 4
+                    var positionOutput = new List<int>();
+                    for (int digit = 0; digit < input.Count; digit++)
+                    {
+                        var patternIndex = ((digit + 1) / outputPosition) % 4;
+                        var patternValue = pattern[patternIndex];
+                        var outputValue = input[digit];
+                        var positionValue = outputValue * patternValue;
+                        if (positionValue > 9)
+                        {
+                            positionValue = positionValue % 10;
+                        }
+                        positionOutput.Add(positionValue);
+                    }
+
+                    var outputForPosition = Math.Abs(positionOutput.Sum());
+                    output.Add(outputForPosition > 9 ? outputForPosition % 10 : outputForPosition);
+                }
+
+                input = output;
+            }
+
+            var s = new StringBuilder();
+            foreach (var x in input.Skip(offset).Take(8))
+            {
+                s.Append(x);
+            }
+            return s.ToString();
+        }
+
         public string FirstStar()
         {
             var input = ReadInput();
@@ -68,10 +107,12 @@ namespace Solutions.Event2019
             return Run(digits, 100);
         }
 
-        public int SecondStar()
+        public string SecondStar()
         {
             var input = ReadInput();
-            return 0;
+            var multipliedInput = string.Join("", Enumerable.Repeat(input, 10_000));
+            var digits = Parse(multipliedInput);
+            return Run2(digits, 100);
         }
 
         [Fact]
@@ -83,7 +124,7 @@ namespace Solutions.Event2019
         [Fact]
         public void SecondStarTest()
         {
-            Assert.Equal(-1, SecondStar());
+            Assert.Equal("", SecondStar());
         }
 
         [Fact]
@@ -102,6 +143,25 @@ namespace Solutions.Event2019
             var input = "12345678";
             var digits = Parse(input);
             var first8 = Run(digits, 2);
+
+            Assert.Equal("34040438", first8);
+        }
+
+        private static int GetPatternValue(int outputPosition, int inputPosition)
+        {
+            
+            return 0;
+        }
+
+        
+
+        [Fact]
+        public void SecondStarExample1()
+        {
+            var input = "03036732577212944063491565474664";
+            var multipliedInput = string.Join("", Enumerable.Repeat(input, 10_000));
+            var digits = Parse(multipliedInput);
+            var first8 = Run2(digits, 100);
 
             Assert.Equal("34040438", first8);
         }
---
+++ Solutions/Event2019/Day18.cs
@@ -0,0 +1,35 @@
+﻿using Xunit;
+using static Solutions.InputReader;
+
+
+namespace Solutions.Event2019
+{
+    // 
+    public class Day18
+    {
+
+        public int FirstStar()
+        {
+            var input = ReadLineInput();
+            return 0;
+        }
+
+        public int SecondStar()
+        {
+            var input = ReadLineInput();
+            return 0;
+        }
+
+        [Fact]
+        public void FirstStarTest()
+        {
+            Assert.Equal(3401852, FirstStar());
+        }
+
+        [Fact]
+        public void SecondStarTest()
+        {
+            Assert.Equal(5099916, SecondStar());
+        }
+    }
+}
