8a415a7de7954f411721aceb85dcb63f520814d6
Matias Jansson <matiasjansson@gmail.com>
2019-12-19 21:24:39 +0100

    Solved 2019 day 16

--- Solutions/Event2019/Day16.cs
+++ Solutions/Event2019/Day16.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using MoreLinq;
 using Xunit;
 using static Solutions.InputReader;
 
@@ -11,22 +12,11 @@ namespace Solutions.Event2019
     // --- Day 16: Flawed Frequency Transmission ---
     public class Day16
     {
-        private static List<int> pattern = new List<int> {0,1,0,-1};
+        private static readonly List<int> Pattern = new List<int> {0,1,0,-1};
 
         private static List<int> Parse(string input)
         {
             return input.Select(x => int.Parse(x.ToString())).ToList();
-        }
-
-        private static List<int> Repeat(List<int> input, int times)
-        {
-            var output = input;
-            foreach (var _ in Enumerable.Range(1,times))
-            {
-                output.Concat(input);
-            }
-
-            return output;
         }
 
         private static string Run(List<int> input, int phases)
@@ -40,18 +30,14 @@ namespace Solutions.Event2019
                     for (int digit = 0; digit < input.Count; digit++)
                     {
                         var patternIndex = ((digit + 1) / (outputPosition)) % 4;
-                        var patternValue = pattern[patternIndex];
+                        var patternValue = Pattern[patternIndex];
                         var outputValue = input[digit];
-                        var positionValue = outputValue * patternValue;
-                        if (positionValue > 9)
-                        {
-                            positionValue = positionValue % 10;
-                        }
+                        var positionValue = (outputValue * patternValue) % 10;
                         positionOutput.Add(positionValue);
                     }
 
                     var outputForPosition = Math.Abs(positionOutput.Sum());
-                    output.Add(outputForPosition > 9 ? outputForPosition % 10 : outputForPosition);
+                    output.Add( outputForPosition % 10);
                 }
 
                 input = output;
@@ -65,42 +51,29 @@ namespace Solutions.Event2019
             return s.ToString();
         }
 
+        private static List<int> Transform(List<int> input)
+        {
+            var accumulated = input.Scan(0, (sum, value) => sum + value).ToList();
+
+            return input.Select((_, i) => (accumulated[accumulated.Count - 1] - accumulated[i]) % 10).ToList();
+        }
+
         private static string Run2(string inputText, int phases, int repeat)
         {
-            //var multipliedInput = string.Join("", Enumerable.Repeat(inputText, 10_000));
-            var input = Parse(inputText);
+            var multipliedInput = string.Join("", Enumerable.Repeat(inputText, repeat));
+            var inputOriginal = Parse(multipliedInput);
 
-            //var offset = int.Parse(string.Join("", input.Take(7).Select(c => int.Parse(c.ToString()))));
-            //input = input.Skip(offset).Take(8).ToList();
+            var offsetInput = string.Join("", inputOriginal.Take(7).Select(c => int.Parse(c.ToString())));
+            var offset = int.Parse(offsetInput);
+            var input = inputOriginal.Skip(offset).ToList();
 
             for (int phase = 1; phase <= phases; phase++)
             {
-                var output = new List<int>();
-                for (int outputPosition = 1; outputPosition <= 1; outputPosition++)
-                {
-                    var positionOutput = new List<int>();
-                    for (int digit = 0; digit < input.Count; digit++)
-                    {
-                        var patternIndex = ((digit + 1) / (outputPosition)) % 4;
-                        var patternValue = pattern[patternIndex];
-                        var outputValue = input[digit];
-                        var positionValue = outputValue * patternValue;
-                        if (positionValue > 9)
-                        {
-                            positionValue = positionValue % 10;
-                        }
-                        positionOutput.Add(positionValue);
-                    }
-
-                    var outputForPosition = Math.Abs(positionOutput.Sum());
-                    output.Add(outputForPosition > 9 ? outputForPosition % 10 : outputForPosition);
-                }
-
-                input = output;
+                input = Transform(input);
             }
 
             var s = new StringBuilder();
-            foreach (var x in input/*.Skip(offset).Take(8)*/)
+            foreach (var x in input.Take(8))
             {
                 s.Append(x);
             }
@@ -129,7 +102,7 @@ namespace Solutions.Event2019
         [Fact]
         public void SecondStarTest()
         {
-            Assert.Equal("", SecondStar());
+            Assert.Equal("37717791", SecondStar());
         }
 
         [Fact]
@@ -153,15 +126,6 @@ namespace Solutions.Event2019
         }
 
         [Fact]
-        public void OffsetTest()
-        {
-            var input = "98765432109876543210";
-            var offset = 7;
-            var result = string.Join("", input.Skip(offset).Take(8).ToList());
-            Assert.Equal("21098765", result);
-        }
-
-        [Fact]
         public void SecondStarExample1()
         {
             var input = "03036732577212944063491565474664";
@@ -173,7 +137,7 @@ namespace Solutions.Event2019
         [Fact]
         public void SecondStarExample2()
         {
-            var input = "03036732577212944063491565474664";
+            var input = "02935109699940807407585447034323";
             var first8 = Run2(input, 100, 10_000);
 
             Assert.Equal("78725270", first8);
@@ -182,7 +146,7 @@ namespace Solutions.Event2019
         [Fact]
         public void SecondStarExample3()
         {
-            var input = "03036732577212944063491565474664";
+            var input = "03081770884921959731165446850517";
             var first8 = Run2(input, 100, 10_000);
 
             Assert.Equal("53553731", first8);
