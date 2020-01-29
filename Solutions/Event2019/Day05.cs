079a223591b4e2c0517721c34634dab46a19d39b
Matias Jansson <matiasjansson@gmail.com>
2020-01-29 14:53:46 +0100

    Refactoring to shared computer

--- Solutions/Event2019/Day05.cs
+++ Solutions/Event2019/Day05.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using Xunit;
 using static Solutions.InputReader;
 
@@ -10,177 +8,20 @@ namespace Solutions.Event2019
     // --- Day 5: Sunny with a Chance of Asteroids ---
     public class Day05
     {
-        public static List<int> Parse(string input)
-        {
-            return input.Split(',').Select(int.Parse).ToList();
-        }
-
-        private enum Mode
-        {
-            Position = 0,
-            Immediate
-        }
-
-        private class Instruction
-        {
-            public Instruction(int operation)
-            {
-                OperationCode = operation % 100;
-                Parameter1Mode = (Mode)((operation / 100) % 10);
-                Parameter2Mode = (Mode)((operation / 1000) % 10);
-            }
-
-            public int OperationCode { get; }
-            public Mode Parameter1Mode { get; }
-            public Mode Parameter2Mode { get; }
-
-
-        }
-
-        public static int Execute(List<int> code, int input)
-        {
-            var output = new List<int>();
-
-            for (int i = 0; i < code.Count;)
-            {
-                var instruction = new Instruction(code[i]);
-
-                switch (instruction.OperationCode)
-                {
-                    case 1:
-                        {
-                            var arg1 = code[i + 1];
-                            var arg2 = code[i + 2];
-                            var writeTo = code[i + 3];
-                            arg1 = instruction.Parameter1Mode == Mode.Position ? code[arg1] : arg1;
-                            arg2 = instruction.Parameter2Mode == Mode.Position ? code[arg2] : arg2;
-                            var result = arg1 + arg2;
-                            code[writeTo] = result;
-                            i = i + 4;
-                            break;
-                        }
-                    case 2:
-                        {
-                            var arg1 = code[i + 1];
-                            var arg2 = code[i + 2];
-                            var writeTo = code[i + 3];
-                            arg1 = instruction.Parameter1Mode == Mode.Position ? code[arg1] : arg1;
-                            arg2 = instruction.Parameter2Mode == Mode.Position ? code[arg2] : arg2;
-                            var result = arg1 * arg2;
-                            code[writeTo] = result;
-                            i = i + 4;
-                            break;
-                        }
-                    case 3:
-                        {
-                            var arg1 = code[i + 1];
-                            code[arg1] = input;
-                            i = i + 2;
-                            break;
-                        }
-                    case 4:
-                        {
-                            var arg1 = code[i + 1];
-                            arg1 = instruction.Parameter1Mode == Mode.Position ? code[arg1] : arg1;
-                            output.Add(arg1);
-                            i = i + 2;
-                            break;
-                        }
-                    case 5:
-                        {
-                            var arg1 = code[i + 1];
-                            var arg2 = code[i + 2];
-                            arg1 = instruction.Parameter1Mode == Mode.Position ? code[arg1] : arg1;
-                            arg2 = instruction.Parameter2Mode == Mode.Position ? code[arg2] : arg2;
-                            if (arg1 != 0)
-                            {
-                                i = arg2;
-                            }
-                            else
-                            {
-                                i = i + 3;
-                            }
-                            break;
-                        }
-                    case 6:
-                        {
-                            var arg1 = code[i + 1];
-                            var arg2 = code[i + 2];
-                            arg1 = instruction.Parameter1Mode == Mode.Position ? code[arg1] : arg1;
-                            arg2 = instruction.Parameter2Mode == Mode.Position ? code[arg2] : arg2;
-                            if (arg1 == 0)
-                            {
-                                i = arg2;
-                            }
-                            else
-                            {
-                                i = i + 3;
-                            }
-                            break;
-                        }
-                    case 7:
-                        {
-                            var arg1 = code[i + 1];
-                            var arg2 = code[i + 2];
-                            var writeTo = code[i + 3];
-
-                            arg1 = instruction.Parameter1Mode == Mode.Position ? code[arg1] : arg1;
-                            arg2 = instruction.Parameter2Mode == Mode.Position ? code[arg2] : arg2;
-                            if (arg1 < arg2)
-                            {
-                                code[writeTo] = 1;
-                            }
-                            else
-                            {
-                                code[writeTo] = 0;
-                            }
-
-                            i = i + 4;
-                            break;
-                        }
-                    case 8:
-                        {
-                            var arg1 = code[i + 1];
-                            var arg2 = code[i + 2];
-                            var writeTo = code[i + 3];
-
-                            arg1 = instruction.Parameter1Mode == Mode.Position ? code[arg1] : arg1;
-                            arg2 = instruction.Parameter2Mode == Mode.Position ? code[arg2] : arg2;
-                            if (arg1 == arg2)
-                            {
-                                code[writeTo] = 1;
-                            }
-                            else
-                            {
-                                code[writeTo] = 0;
-                            }
-
-                            i = i + 4;
-                            break;
-                        }
-                    case 99:
-                        {
-                            return output.Last();
-                        }
-                    default:
-                        throw new InvalidOperationException($"Unknown op '{instruction.OperationCode}' code at i = {i}");
-                }
-            }
-            throw new InvalidOperationException("Unexpected program exit");
-        }
-
-        public int FirstStar()
+        public long FirstStar()
         {
             var input = ReadInput();
-            var code = Parse(input);
-            return Execute(code, 1);
+            var code = IntCodeComputer.Load(input, 1);
+            code.Execute();
+            return code.Output.Last();
         }
 
-        public int SecondStar()
+        public long SecondStar()
         {
             var input = ReadInput();
-            var code = Parse(input);
-            return Execute(code, 5);
+            var code = IntCodeComputer.Load(input, 5);
+            code.Execute();
+            return code.Output.Last();
         }
 
         [Fact]
