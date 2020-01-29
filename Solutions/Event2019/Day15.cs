8b9d1e8c8bda15db85795c0f383ffeacd0c003fd
Matias Jansson <matiasjansson@gmail.com>
2020-01-29 09:26:55 +0100

    Refactoring to use shared computer

--- Solutions/Event2019/Day15.cs
+++ Solutions/Event2019/Day15.cs
@@ -21,256 +21,6 @@ namespace Solutions.Event2019
         {
             this.output = output;
         }
-        public static List<long> Parse(string input)
-        {
-            return input.Split(',').Select(long.Parse).ToList();
-        }
-
-        private class IntCodeComputer
-        {
-            public enum ExecutionState { WaitingForInput, Ready }
-            private enum Mode { Position = 0, Immediate, Relative }
-            private enum ParameterType { Read, Write }
-
-            private class Instruction
-            {
-                public Instruction(long operation)
-                {
-                    OperationCode = operation % 100;
-                    ParameterModes = new[]
-                    {
-                        (Mode)((operation / 100) % 10),
-                        (Mode)((operation / 1000) % 10),
-                        (Mode)((operation / 10000) % 10),
-                    };
-                }
-
-                public long OperationCode { get; }
-
-                // mode for parameter 1 at index 0
-                // mode for parameter 2 at index 1 
-                // mode for parameter 3 at index 2
-                public Mode[] ParameterModes { get; }
-            }
-
-            private readonly Dictionary<long, long> memory;
-
-            private readonly List<long> input;
-
-            private long instructionPointer;
-            //private int inputPosition;
-            private long relativeBase;
-            private Instruction instruction;
-
-            private Dictionary<long, long> Load(List<long> code)
-            {
-                var m = new Dictionary<long, long>();
-                for (int i = 0; i < code.Count; i++)
-                {
-                    m.Add(i, code[i]);
-                }
-
-                return m;
-            }
-            public IntCodeComputer(List<long> program)
-            {
-                this.memory = Load(program);
-                this.input = new List<long>();
-                this.instructionPointer = 0;
-                //this.inputPosition = 0;
-
-                this.Output = new List<long>();
-
-            }
-
-            public IntCodeComputer(List<long> program, long input)
-            {
-                this.memory = Load(program);
-                this.input = new List<long> { input };
-                this.Output = new List<long>();
-                this.instructionPointer = 0;
-                //this.inputPosition = 0;
-            }
-
-
-            public List<long> Output { get; }
-
-            private long ReadMemory(long address)
-            {
-                if (address < 0)
-                {
-                    throw new ArgumentOutOfRangeException($"Cannot access negative memory address: {address}");
-                }
-                if (!memory.ContainsKey(address))
-                {
-                    memory.Add(address, 0);
-                }
-
-                return memory[address];
-            }
-
-            private long Parameter(long position, Mode mode, ParameterType type)
-            {
-                var value = ReadMemory(instructionPointer + position);
-
-                switch (mode)
-                {
-                    case Mode.Position:
-                        {
-                            return type == ParameterType.Read ? ReadMemory(value) : value;
-                        }
-                    case Mode.Immediate:
-                        {
-                            return value;
-                        }
-                    case Mode.Relative:
-                        {
-                            return type == ParameterType.Read ? ReadMemory(value + relativeBase) : value + relativeBase;
-                        }
-                    default:
-                        throw new ArgumentOutOfRangeException($"Unexpected mode parameter {mode}");
-                }
-            }
-
-            private (long, long, long) LoadParameters(params ParameterType[] parameterTypes)
-            {
-                if (parameterTypes.Length > 3)
-                {
-                    throw new ArgumentOutOfRangeException($"Maximum 3 parameters supported (got {parameterTypes.Length}");
-                }
-
-                var result = new long[3];
-
-                for (long i = 0; i < parameterTypes.Length; i++)
-                {
-                    var mode = instruction.ParameterModes[i];
-                    var type = parameterTypes[i];
-
-                    result[i] = Parameter(i + 1, mode, type);
-                }
-
-                return (result[0], result[1], result[2]);
-            }
-
-            public ExecutionState Execute(int input)
-            {
-                this.input.Add(input);
-                ExecutionState state = Execute();
-                //if (Output.Last() == 2)
-                //{
-                //    throw new Exception("Found oxygen");
-                //}
-
-                if (state == ExecutionState.Ready)
-                {
-                    throw new Exception("Terminated");
-                }
-                return state;
-            }
-
-            public ExecutionState Execute()
-            {
-                while (true)
-                {
-                    instruction = new Instruction(ReadMemory(instructionPointer));
-
-                    switch (instruction.OperationCode)
-                    {
-                        case 1:
-                            {
-                                var (arg1, arg2, writeTo) = LoadParameters(ParameterType.Read, ParameterType.Read, ParameterType.Write);
-
-                                memory[writeTo] = arg1 + arg2;
-                                instructionPointer = instructionPointer + 4;
-                                break;
-                            }
-                        case 2:
-                            {
-                                var (arg1, arg2, writeTo) = LoadParameters(ParameterType.Read, ParameterType.Read, ParameterType.Write);
-
-                                memory[writeTo] = arg1 * arg2;
-                                instructionPointer = instructionPointer + 4;
-                                break;
-                            }
-                        case 3:
-                            {
-                                var (arg1, _, _) = LoadParameters(ParameterType.Write);
-
-                                memory[arg1] = input.Last();//input[inputPosition++];
-                                instructionPointer = instructionPointer + 2;
-                                break;
-                            }
-                        case 4:
-                            {
-                                var (arg1, _, _) = LoadParameters(ParameterType.Read);
-
-                                Output.Add(arg1);
-                                instructionPointer = instructionPointer + 2;
-                                return ExecutionState.WaitingForInput;
-                            }
-                        case 5:
-                            {
-                                var (arg1, arg2, _) = LoadParameters(ParameterType.Read, ParameterType.Read);
-
-                                instructionPointer = arg1 != 0 ? arg2 : instructionPointer + 3;
-                                break;
-                            }
-                        case 6:
-                            {
-                                var (arg1, arg2, _) = LoadParameters(ParameterType.Read, ParameterType.Read);
-
-                                instructionPointer = arg1 == 0 ? arg2 : instructionPointer + 3;
-                                break;
-                            }
-                        case 7:
-                            {
-                                var (arg1, arg2, writeTo) = LoadParameters(ParameterType.Read, ParameterType.Read, ParameterType.Write);
-
-                                memory[writeTo] = arg1 < arg2 ? 1 : 0;
-
-                                instructionPointer = instructionPointer + 4;
-                                break;
-                            }
-                        case 8:
-                            {
-                                var (arg1, arg2, writeTo) = LoadParameters(ParameterType.Read, ParameterType.Read, ParameterType.Write);
-
-                                memory[writeTo] = arg1 == arg2 ? 1 : 0;
-
-                                instructionPointer = instructionPointer + 4;
-                                break;
-                            }
-                        case 9:
-                            {
-                                var (arg1, _, _) = LoadParameters(ParameterType.Read);
-
-                                relativeBase += arg1;
-
-                                instructionPointer = instructionPointer + 2;
-                                break;
-                            }
-                        case 99:
-                            {
-                                return ExecutionState.Ready;
-                            }
-                        default:
-                            throw new InvalidOperationException(
-                                $"Unknown op code '{instruction.OperationCode}' at address = {instructionPointer}");
-                    }
-                }
-            }
-
-            public List<long> ExecuteAll()
-            {
-                var state = ExecutionState.WaitingForInput;
-                while (state != ExecutionState.Ready)
-                {
-                    state = Execute();
-                }
-
-                return Output;
-            }
-        }
 
         private static int OppositeDirection(int code)
         {
@@ -390,7 +140,7 @@ namespace Solutions.Event2019
 
                 if (!knowns.ContainsKey(next))
                 {
-                    var executionState = computer.Execute(d);
+                    computer.Execute(d);
                     var output = computer.Output.Last();
                     knowns.Add(next, output);
 
@@ -480,18 +230,18 @@ namespace Solutions.Event2019
             }
         }
 
-        private int FewestMovementCommandsToOxygenSystem(List<long> program)
+        private int FewestMovementCommandsToOxygenSystem(string program)
         {
-            var computer = new IntCodeComputer(program);
+            var computer = IntCodeComputer.Load(program);
 
             var depthFirstPath = Explore(new Point(0,0), computer, new Dictionary<Point, long>());
 
             return depthFirstPath.Last().Depth;
         }
 
-        private int TimeToFillWithOxygen(List<long> program)
+        private int TimeToFillWithOxygen(string program)
         {
-            var computer = new IntCodeComputer(program);
+            var computer = IntCodeComputer.Load(program);
 
             var map = new Dictionary<Point, long>();
             var depthFirstPath = Explore(new Point(0, 0), computer, map);
@@ -514,8 +264,7 @@ namespace Solutions.Event2019
         public static void ExplorerMode()
         {
             var input = ReadInput();
-            var program = Parse(input);
-            var computer = new IntCodeComputer(program);
+            var computer = IntCodeComputer.Load(input);
             var visited = new Dictionary<Point, int>();
             var currentLocation = new Point(0,0);
             PrintSurrounding(currentLocation, visited);
@@ -593,15 +342,13 @@ namespace Solutions.Event2019
         public int FirstStar()
         {
             var input = ReadInput();
-            var program = Parse(input);
-            return FewestMovementCommandsToOxygenSystem(program);
+            return FewestMovementCommandsToOxygenSystem(input);
         }
 
         public int SecondStar()
         {
             var input = ReadInput();
-            var program = Parse(input);
-            return TimeToFillWithOxygen(program);
+            return TimeToFillWithOxygen(input);
         }
 
         [Fact]
