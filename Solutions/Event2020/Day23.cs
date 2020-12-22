4e0c48e1bf6dd98bcf04084edf17049287146eba
Matias Jansson <matias.jansson@noretech.se>
2020-12-22 21:34:33 +0100

    Prepare day 23 of 2020

---
+++ Solutions/Event2020/Day23.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using Xunit.Abstractions;
+using static Solutions.InputReader;
+
+
+namespace Solutions.Event2020
+{
+    // 
+
+    public class Day23
+    {
+        private readonly ITestOutputHelper output;
+
+        public Day23(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        public int FirstStar()
+        {
+            var input = ReadLineInput().ToList();
+            
+            return 0;
+        }
+
+        public int SecondStar()
+        {
+            var input = ReadLineInput().ToList();
+            
+            return 0;
+        }
+
+        [Fact]
+        public void FirstStarTest()
+        {
+            Assert.Equal(-1, FirstStar());
+        }
+
+        [Fact]
+        public void SecondStarTest()
+        {
+            var result = SecondStar();
+            Assert.Equal(-1, result);
+        }
+    }
+}
