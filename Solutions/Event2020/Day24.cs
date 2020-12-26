7a415f11361e12542dc45a8115fe2bad410c9445
Matias Jansson <matias.jansson@noretech.se>
2020-12-26 21:48:47 +0100

    Solved day 24 of 2020

--- Solutions/Event2020/Day24.cs
+++ Solutions/Event2020/Day24.cs
@@ -81,48 +81,69 @@ namespace Solutions.Event2020
 
         private Point Move(List<Direction> directions)
         {
-            Point current = new Point(0,0);
+            Point current = new Point(0, 0);
 
             foreach (var direction in directions)
             {
-                switch (direction)
-                {
-                    case Direction.NorthEast:
-                        {
-                            var x = current.Y % 2 == 0 ? 1 : 0;
-                            current = new Point(current.X + x, current.Y + 1);
-                            break;
-                        }
-                    case Direction.East:
-                        current = new Point(current.X + 1, current.Y);
-                        break;
-                    case Direction.SouthEast:
-                        {
-                            var x = current.Y % 2 == 0 ? 1 : 0;
-                            current = new Point(current.X + x, current.Y - 1);
-                            break;
-                        }
-                    case Direction.SouthWest:
-                        {
-                            var x = current.Y % 2 == 0 ? 0 : -1;
-                            current = new Point(current.X + x, current.Y - 1);
-                            break;
-                        }
-                    case Direction.West:
-                        current = new Point(current.X - 1, current.Y);
-                        break;
-                    case Direction.NorthWest:
-                        {
-                            var x = current.Y % 2 == 0 ? 0 : -1;
-                            current = new Point(current.X + x, current.Y + 1);
-                            break;
-                        }
-                    default:
-                        throw new ArgumentOutOfRangeException($"Unexpected direction: {direction}");
-                }
+                current = Move(current, direction);
             }
 
             return current;
+        }
+
+        private Point Move(Point from, Direction direction)
+        {
+            switch (direction)
+            {
+                case Direction.NorthEast:
+                    {
+                        var x = from.Y % 2 == 0 ? 1 : 0;
+                        from = new Point(from.X + x, from.Y + 1);
+                        break;
+                    }
+                case Direction.East:
+                    from = new Point(from.X + 1, from.Y);
+                    break;
+                case Direction.SouthEast:
+                    {
+                        var x = from.Y % 2 == 0 ? 1 : 0;
+                        from = new Point(from.X + x, from.Y - 1);
+                        break;
+                    }
+                case Direction.SouthWest:
+                    {
+                        var x = from.Y % 2 == 0 ? 0 : -1;
+                        from = new Point(from.X + x, from.Y - 1);
+                        break;
+                    }
+                case Direction.West:
+                    from = new Point(from.X - 1, from.Y);
+                    break;
+                case Direction.NorthWest:
+                    {
+                        var x = from.Y % 2 == 0 ? 0 : -1;
+                        from = new Point(from.X + x, from.Y + 1);
+                        break;
+                    }
+                default:
+                    throw new ArgumentOutOfRangeException($"Unexpected direction: {direction}");
+            }
+
+            return from;
+        }
+
+        private static readonly List<Direction> Directions = new List<Direction> {
+            Direction.NorthEast,
+            Direction.East,
+            Direction.SouthEast,
+            Direction.SouthWest,
+            Direction.West,
+            Direction.NorthWest
+        };
+
+        public List<Point> Adjacent(Point to)
+        {
+            return Directions.Select(direction => Move(to, direction)).ToList();
         }
 
         private int CountBlackTiles(List<List<Direction>> directions)
@@ -146,6 +167,58 @@ namespace Solutions.Event2020
             return turned.Values.Where(x => x % 2 != 0).Count();
         }
 
+        private int CountBlackTilesAfterDays(List<List<Direction>> directions, int days)
+        {
+            Dictionary<Point, bool> turned = new Dictionary<Point, bool>();
+
+            foreach (var line in directions)
+            {
+                var destination = Move(line);
+
+                if (turned.ContainsKey(destination))
+                {
+                    turned[destination] = !turned[destination];
+                }
+                else
+                {
+                    turned.Add(destination, true);
+                }
+            }
+
+            var previousBlackTiles = new HashSet<Point>(turned.Where(t => t.Value).Select(t => t.Key));
+
+            foreach (var i in Enumerable.Range(1, 100))
+            {
+                var nextBlackTiles = new HashSet<Point>();
+
+                foreach (var blackTile in previousBlackTiles)
+                {
+                    var adjacent = Adjacent(blackTile);
+
+                    var adjacentBlacks = adjacent.Where(a => previousBlackTiles.Contains(a)).Count();
+
+                    var turn = adjacentBlacks == 0 || adjacentBlacks > 2;
+                    if (!turn)
+                    {
+                        nextBlackTiles.Add(blackTile);
+                    }
+
+                    foreach (var adjacentWhiteTile in adjacent.Where(a => !previousBlackTiles.Contains(a)))
+                    {
+                        var adjacentToWhite = Adjacent(adjacentWhiteTile);
+                        var adjacentToWhiteBlacks = adjacentToWhite.Where(a => previousBlackTiles.Contains(a)).Count();
+                        if (adjacentToWhiteBlacks == 2)
+                        {
+                            nextBlackTiles.Add(adjacentWhiteTile);
+                        }
+                    }
+                }
+
+                previousBlackTiles = nextBlackTiles;
+            }
+            return previousBlackTiles.Count;
+        }
+
         public int FirstStar()
         {
             var input = ReadLineInput().ToList();
@@ -158,13 +231,18 @@ namespace Solutions.Event2020
 
         public int SecondStar()
         {
-            return 0;
+            var input = ReadLineInput().ToList();
+            var directions = Parse(input);
+
+            var count = CountBlackTilesAfterDays(directions, 100);
+
+            return count;
         }
 
         [Fact]
         public void FirstStarTest()
         {
-            Assert.Equal(-1, FirstStar());
+            Assert.Equal(391, FirstStar());
         }
 
         [Fact]
@@ -217,6 +295,40 @@ namespace Solutions.Event2020
             var count = CountBlackTiles(directions);
 
             Assert.Equal(10, count);
+        }
+
+        [Fact]
+        public void SecondStarExample()
+        {
+            var input = new List<string>
+            {
+                "sesenwnenenewseeswwswswwnenewsewsw",
+                "neeenesenwnwwswnenewnwwsewnenwseswesw",
+                "seswneswswsenwwnwse",
+                "nwnwneseeswswnenewneswwnewseswneseene",
+                "swweswneswnenwsewnwneneseenw",
+                "eesenwseswswnenwswnwnwsewwnwsene",
+                "sewnenenenesenwsewnenwwwse",
+                "wenwwweseeeweswwwnwwe",
+                "wsweesenenewnwwnwsenewsenwwsesesenwne",
+                "neeswseenwwswnwswswnw",
+                "nenwswwsewswnenenewsenwsenwnesesenew",
+                "enewnwewneswsewnwswenweswnenwsenwsw",
+                "sweneswneswneneenwnewenewwneswswnese",
+                "swwesenesewenwneswnwwneseswwne",
+                "enesenwswwswneneswsenwnewswseenwsese",
+                "wnwnesenesenenwwnenwsewesewsesesew",
+                "nenewswnwewswnenesenwnesewesw",
+                "eneswnwswnwsenenwnwnwwseeswneewsenese",
+                "neswnwewnwnwseenwseesewsenwsweewe",
+                "wseweeenwnesenwwwswnew"
+            };
+
+            var directions = Parse(input);
+
+            var count = CountBlackTilesAfterDays(directions, 100);
+
+            Assert.Equal(2208, count);
         }
     }
 }
