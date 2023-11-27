b18a38e778cd98418cecd9941d44fa706a45120f
Matias Jansson <matias.jansson@noretech.se>
2023-11-27 19:57:11 +0100

    Solved day 1 of 2022

---
+++ Solutions/Event2022/Day01.cs
@@ -0,0 +1,110 @@
+﻿using MoreLinq;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using Xunit.Abstractions;
+using static Solutions.InputReader;
+
+
+namespace Solutions.Event2022
+{
+    // --- Day 1: Calorie Counting ---
+    public class Day01
+    {
+        private readonly ITestOutputHelper output;
+
+        public Day01(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        private int CountMostCalories(IList<string> input)
+        {
+            return input
+                .Split(string.IsNullOrEmpty)
+                .Select(elf => elf.Select(int.Parse).Sum())
+                .Max();
+        }
+        private int CountTopThreeMostCalories(IList<string> input)
+        {
+            return input
+                .Split(string.IsNullOrEmpty)
+                .Select(elf => elf.Select(int.Parse).Sum())
+                .OrderByDescending(x => x)
+                .Take(3)
+                .Sum();
+        }
+
+        public int FirstStar()
+        {
+            var input = ReadLineInput();
+            return CountMostCalories(input);
+        }
+
+        public int SecondStar()
+        {
+            var input = ReadLineInput();
+            return CountTopThreeMostCalories(input);
+        }
+
+        [Fact]
+        public void FirstStarTest()
+        {
+            Assert.Equal(69912, FirstStar());
+        }
+
+        [Fact]
+        public void SecondStarTest()
+        {
+            Assert.Equal(208180, SecondStar());
+        }
+
+        [Fact]
+        public void FirstStarExample()
+        {
+            var example = new List<string>
+            {
+                "1000",
+                "2000",
+                "3000",
+                "",
+                "4000",
+                "",
+                "5000",
+                "6000",
+                "",
+                "7000",
+                "8000",
+                "9000",
+                "",
+                "10000"
+            };
+
+            Assert.Equal(24000, CountMostCalories(example));
+        }
+
+        [Fact]
+        public void SecondStarExample()
+        {
+            var example = new List<string>
+            {
+                "1000",
+                "2000",
+                "3000",
+                "",
+                "4000",
+                "",
+                "5000",
+                "6000",
+                "",
+                "7000",
+                "8000",
+                "9000",
+                "",
+                "10000"
+            };
+
+            Assert.Equal(45000, CountTopThreeMostCalories(example));
+        }
+    }
+}
