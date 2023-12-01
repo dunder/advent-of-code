b932433dc322ede12a1bc008d5e4125e72917f6d
Matias Jansson <matias.jansson@noretech.se>
2023-12-01 08:36:48 +0100

    Solved day 1 of 2023

--- Solutions/Event2023/Day01.cs
+++ Solutions/Event2023/Day01.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿
 using System.Collections.Generic;
 using System.Linq;
 using Xunit;
@@ -8,7 +8,7 @@ using static Solutions.InputReader;
 
 namespace Solutions.Event2023
 {
-    // --- Day X: Phrase ---
+    // --- Day 1: Trebuchet?! ---
     public class Day01
     {
         private readonly ITestOutputHelper output;
@@ -18,40 +18,138 @@ namespace Solutions.Event2023
             this.output = output;
         }
 
+        private int SumOfCalibrationValues(IList<string> input)
+        {
+            return input.Select(text =>
+            {
+                var x = text.First(char.IsDigit);
+                var y = text.Last(char.IsDigit);
+
+                return int.Parse(string.Join("", x, y));
+            }
+            ).Sum();
+        }
+
+        private static string ToDigit(string number) => number switch
+        {
+            "one" => "1",
+            "two" => "2",
+            "three" => "3",
+            "four" => "4",
+            "five" => "5",
+            "six" => "6",
+            "seven" => "7",
+            "eight" => "8",
+            "nine" =>  "9",
+            _ => number,
+        };
+
+        private static List<string> numbers = new List<string> { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+        private static bool TryParseDigit(string text, out string digit) {
+            var found = numbers.FirstOrDefault(n => text.StartsWith(n));
+            if (found != null)
+            {
+                digit = ToDigit(found);
+                return true;
+            }
+            digit = default;
+            return false;
+        }
+
+        private int SumOfCalibrationValuesWithLetters(IList<string> input)
+        {
+            return input.Select(text =>
+            {
+                string first = "";
+                for (int i = 0; i < text.Length; i++)
+                {
+                    var c = text[i];
+                    var rest = text.Substring(i);
+                    if (char.IsDigit(c))
+                    {
+                        first = c.ToString();
+                        break;
+                    }
+                    if (TryParseDigit(rest, out first))
+                    {
+                        break;
+                    }
+                }
+
+                string last = "";
+                for (int i = text.Length - 1; i >= 0; i--)
+                {
+                    var c = text[i];
+                    var rest = text.Substring(i);
+                    if (char.IsDigit(c))
+                    {
+                        last = c.ToString();
+                        break;
+                    }
+                    if (TryParseDigit(rest, out last))
+                    {
+                        break;
+                    }
+                }
+
+                return int.Parse(first + last);
+            }
+            ).Sum();
+        }
         public int FirstStar()
         {
             var input = ReadLineInput();
-            return 0;
+            return SumOfCalibrationValues(input);
         }
 
         public int SecondStar()
         {
             var input = ReadLineInput();
-            return 0;
+            return SumOfCalibrationValuesWithLetters(input);
         }
 
         [Fact]
         public void FirstStarTest()
         {
-            Assert.Equal(-1, FirstStar());
+            Assert.Equal(55386, FirstStar());
         }
 
         [Fact]
         public void SecondStarTest()
         {
-            Assert.Equal(-1, SecondStar());
+            Assert.Equal(54824, SecondStar());
         }
 
         [Fact]
         public void FirstStarExample()
         {
-            
+            var example = new List<string>
+            {
+                "1abc2",
+                "pqr3stu8vwx",
+                "a1b2c3d4e5f",
+                "treb7uchet"
+            };
+
+            Assert.Equal(142, SumOfCalibrationValues(example));
         }
 
         [Fact]
         public void SecondStarExample()
         {
-            
+             var example = new List<string>
+            {
+                "two1nine",
+                "eightwothree",
+                "abcone2threexyz",
+                "xtwone3four",
+                "4nineeightseven2",
+                "zoneight234",
+                "7pqrstsixteen"
+            };
+
+            Assert.Equal(281, SumOfCalibrationValuesWithLetters(example));
         }
     }
 }
