36ca357b099e51291c6626b9f42a2bfe1839104f
Matias Jansson <matias.jansson@noretech.se>
2023-12-03 20:01:01 +0100

    Solved day 3 of 2023

--- Solutions/Event2023/Day03.cs
+++ Solutions/Event2023/Day03.cs
@@ -1,57 +1,189 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using Xunit;
-using Xunit.Abstractions;
 using static Solutions.InputReader;
 
 
 namespace Solutions.Event2023
 {
-    // --- Day X: Phrase ---
+    // --- Day 3: Gear Ratios ---
     public class Day03
     {
-        private readonly ITestOutputHelper output;
-
-        public Day03(ITestOutputHelper output)
+        private record Position(int X, int Y);
+        private record PartNumber(int Id, HashSet<Position> Positions) { }
+        private record Symbol(char symbol, Position Position);
+        private record EngineSchematic(int Rows, int Columns, List<PartNumber> Parts, List<Symbol> Symbols)
         {
-            this.output = output;
+            public int SumPartNumbers()
+            {
+                HashSet<Position> symbolPositions = Symbols.Select(symbol => symbol.Position).ToHashSet();
+
+                bool IsAdjacentToSymbol(PartNumber part)
+                {
+                    return PositionsAdjacentTo(part).Any(symbolPositions.Contains);
+                }
+
+                return Parts
+                    .Where(IsAdjacentToSymbol)
+                    .Select(x => x.Id)
+                    .Sum();
+            }
+
+            public int GearRatio()
+            {
+                return Gears
+                    .Select(PartsAdjacentTo)
+                    .Where(adjacent => adjacent.Count() == 2)
+                    .Select(parts => parts.Aggregate(1, (ratio, part) => ratio * part.Id))
+                    .Sum();
+            }
+
+            private List<PartNumber> PartsAdjacentTo(Symbol symbol)
+            {
+                return Parts
+                    .Where(part => part.Positions.Intersect(PositionsAdjacentTo(symbol.Position)).Any())
+                    .ToList();
+            }
+
+            private HashSet<Position> PositionsAdjacentTo(PartNumber part)
+            {
+                return part.Positions.Select(PositionsAdjacentTo).SelectMany(x => x).ToHashSet();
+            }
+
+            private HashSet<Position> PositionsAdjacentTo(Position position)
+            {
+                var adjacent = new List<Position>
+                {
+                    new Position(position.X, position.Y - 1),
+                    new Position(position.X + 1, position.Y - 1),
+                    new Position(position.X + 1, position.Y),
+                    new Position(position.X + 1, position.Y + 1),
+                    new Position(position.X, position.Y + 1),
+                    new Position(position.X - 1, position.Y + 1),
+                    new Position(position.X - 1, position.Y),
+                    new Position(position.X - 1, position.Y - 1)
+                };
+
+                return new HashSet<Position>(
+                    adjacent.Where(
+                        position => position.X >= 0 &&
+                        position.X < Columns &&
+                        position.Y >= 0 && position.X < Rows
+                    ));
+            }
+
+            private IEnumerable<Symbol> Gears => Symbols.Where(symbol => symbol.symbol == '*');
+        }
+
+        private EngineSchematic Parse(IList<string> input)
+        {
+            int rows = input.Count;
+            int columns = input.First().Length;
+
+            var parts = new List<PartNumber>();
+            var symbols = new List<Symbol>();
+
+            for (int y = 0; y < rows; y++)
+            {
+                var row = input[y];
+
+                for (int x = 0; x < columns; x++)
+                {
+                    char column = row[x];
+
+                    if (column == '.') continue;
+
+                    PartNumber ParsePartNumber() {
+                        var number = "";
+                        var positions = new HashSet<Position>();
+                        do {
+                            positions.Add(new Position(x, y));
+                            var digit = row[x];
+                            number += digit;
+                            x++;
+                        } while (x < columns && char.IsDigit(row[x]));
+                        x--;
+                        return new PartNumber(int.Parse(number), positions);
+                    }
+
+                    if (char.IsDigit(column))
+                    {
+                        parts.Add(ParsePartNumber());
+                    }
+                    else
+                    {
+                        symbols.Add(new Symbol(column, new Position(x, y)));
+                    }
+                }
+            }
+
+            return new EngineSchematic(rows, columns, parts, symbols);
         }
 
         public int FirstStar()
         {
             var input = ReadLineInput();
-            return 0;
+
+            return Parse(input).SumPartNumbers();
         }
 
         public int SecondStar()
         {
             var input = ReadLineInput();
-            return 0;
+
+            return Parse(input).GearRatio();
         }
 
         [Fact]
         public void FirstStarTest()
         {
-            Assert.Equal(-1, FirstStar());
+            Assert.Equal(532331, FirstStar());
         }
 
         [Fact]
         public void SecondStarTest()
         {
-            Assert.Equal(-1, SecondStar());
+            Assert.Equal(82301120, SecondStar());
         }
 
         [Fact]
         public void FirstStarExample()
         {
-            
+            var example = new List<string>
+            {
+                "467..114..",
+                "...*......",
+                "..35..633.",
+                "......#...",
+                "617*......",
+                ".....+.58.",
+                "..592.....",
+                "......755.",
+                "...$.*....",
+                ".664.598..",
+            };
+
+            Assert.Equal(4361, Parse(example).SumPartNumbers());
         }
 
         [Fact]
         public void SecondStarExample()
         {
-            
+            var example = new List<string>
+            {
+                "467..114..",
+                "...*......",
+                "..35..633.",
+                "......#...",
+                "617*......",
+                ".....+.58.",
+                "..592.....",
+                "......755.",
+                "...$.*....",
+                ".664.598..",
+            };
+
+            Assert.Equal(467835, Parse(example).GearRatio());
         }
     }
 }
