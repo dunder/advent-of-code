f46b42d8b2754df19ae5eb50b00f4f0c608cdbc0
Matias Jansson <matias.jansson@noretech.se>
2023-12-18 23:52:44 +0100

    Work in progress problem 2, day 18 of 2023

--- Solutions/Event2023/Day18.cs
+++ Solutions/Event2023/Day18.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using Xunit;
 using Xunit.Abstractions;
-using static Solutions.Event2018.Day24;
 using static Solutions.InputReader;
 
 
@@ -46,6 +47,31 @@ namespace Solutions.Event2023
             }).ToList();
         }
 
+        private List<Draw> Parse2(IList<string> input)
+        {
+            var colorRegex = new Regex(@"[0-9a-f]+");
+
+            return input.Select(line =>
+            {
+                var parts = line.Split(" ");
+                
+                var color = colorRegex.Match(parts[2]).Value;
+
+                int steps = Convert.ToInt32($"0x{color[0..5]}", 16);
+
+                var direction = int.Parse(color[5].ToString()) switch
+                {
+                    3 => Direction.Up,
+                    0 => Direction.Right,
+                    1 => Direction.Down,
+                    2 => Direction.Left,
+                    _ => throw new InvalidOperationException($"Unkown direction : {color}")
+                };
+
+                return new Draw(direction, steps, color);
+            }).ToList();
+        }
+
         private (int,int) Move((int, int) position, Draw draw)
         {
             (int x, int y) = position;
@@ -60,6 +86,25 @@ namespace Solutions.Event2023
                     return (x, y + 1);
                 case Direction.Left:
                     return (x - 1, y);
+                default:
+                    throw new InvalidOperationException($"Unknown direction: {draw.Direction}");
+            }
+        }
+
+        private (int,int) Move2((int, int) position, Draw draw)
+        {
+            (int x, int y) = position;
+
+            switch(draw.Direction)
+            {
+                case Direction.Up:
+                    return (x, y - draw.Steps);
+                case Direction.Right:
+                    return (x + draw.Steps, y);
+                case Direction.Down:
+                    return (x, y + draw.Steps);
+                case Direction.Left:
+                    return (x - draw.Steps, y);
                 default:
                     throw new InvalidOperationException($"Unknown direction: {draw.Direction}");
             }
@@ -163,6 +208,111 @@ namespace Solutions.Event2023
             return visited.Count + inside.Count;
         }
 
+
+        private record VerticalInterval(int Column, int RowTop, int RowBottom)
+        {
+            public int Length => Math.Abs(RowTop - RowBottom) + 1;
+
+            public bool Overlaps(int yTop, int yBottom)
+            {
+                return yTop >= RowTop && yBottom <= RowBottom;
+            }
+        }
+
+        private long DugOut2(List<Draw> digPlan)
+        {
+            var position = (0, 0);
+
+            long borderCount = 0;
+
+            List<VerticalInterval> intervals = new();
+
+            bool IsUTurn(int i)
+            {
+                var previous = i == 0 ? digPlan[^1] : digPlan[i - 1];
+                var next = i == digPlan.Count - 1 ? digPlan[0] : digPlan[i + 1];
+
+                return previous.Direction != next.Direction;
+            }
+
+            for (var i = 0; i < digPlan.Count; i++)
+            {
+                Draw instruction = digPlan[i];
+
+                borderCount += instruction.Steps;
+                
+                switch (instruction.Direction)
+                {
+                    case Direction.Up:
+                        if (instruction.Steps > 1)
+                        {
+                            intervals.Add(new VerticalInterval(position.Item1, position.Item2 - instruction.Steps + 1, position.Item2 - 1));
+                        }
+                        break;
+                    case Direction.Down:
+                        if (instruction.Steps > 1)
+                        {
+                            intervals.Add(new VerticalInterval(position.Item1, position.Item2 + 1, position.Item2 + instruction.Steps - 1));
+                        }
+                        break;
+                    case Direction.Right:
+                        if (!IsUTurn(i))
+                        {
+                            intervals.Add(new VerticalInterval(position.Item1, position.Item2, position.Item2));
+
+                        }
+                        break;
+                    case Direction.Left:
+                        if (!IsUTurn(i))
+                        {
+                            intervals.Add(new VerticalInterval(position.Item1, position.Item2, position.Item2));
+                        }
+                        break;
+                }
+                
+                position = Move2(position, instruction);
+            }
+
+            var columnOrderIntevals = intervals.OrderBy(interval => interval.Column).ToList();
+
+            var horizontalLines = new HashSet<int>();
+
+            foreach (var interval in intervals)
+            {
+                horizontalLines.Add(interval.RowTop);
+                horizontalLines.Add(interval.RowBottom);
+            }
+
+            var sortedHorizontalLines = horizontalLines.OrderBy(x => x).ToList();
+
+            long insideCount = 0;
+
+            for (int i = 1; i < sortedHorizontalLines.Count; i++)
+            {
+                var yTop = sortedHorizontalLines[i-1];
+                var yBottom = sortedHorizontalLines[i]-1;
+
+                long height = Math.Abs(yBottom - yTop) + 1;
+
+                var columnOrdered = intervals.Where(interval => interval.Overlaps(yTop, yBottom)).OrderBy(interval => interval.Column).ToList();
+
+                for (int j = 1; j < columnOrdered.Count; j++)
+                {
+                    if (j % 2 != 0)
+                    {
+                        var x1 = columnOrdered[j - 1].Column;
+                        var x2 = columnOrdered[j].Column;
+
+                        long width = Math.Abs(x1 - x2) + 1;
+
+                        insideCount += height * width;
+                    }
+                }
+            }
+
+            return borderCount + insideCount;
+        }
+
         private void DrawMap(HashSet<(int, int)> visited, HashSet<(int, int)> inside)
         {
             var top = visited.Select(x => x.Item2).Min();
@@ -202,18 +352,16 @@ namespace Solutions.Event2023
             return DugOut(input);
         }
 
-        public int SecondStar()
+        public long SecondStar()
         {
             var input = ReadLineInput();
-            return 0;
+            List<Draw> digPlan = Parse2(input);
+            return DugOut2(digPlan);
         }
 
         [Fact]
         public void FirstStarTest()
         {
-            // 22488 your answer is too low
-            // 22585 your answer is too low
-            // 28168 That's not the right answer.
             Assert.Equal(28911, FirstStar());
         }
 
@@ -269,8 +417,10 @@ namespace Solutions.Event2023
                 "U 2 (#7a21e3)"
             };
 
-            Assert.Equal(952408144115, DugOut(example));
+            var digPlan = Parse(example);
+            
 
+            Assert.Equal(952408144115, DugOut2(digPlan));
         }
     }
 }
