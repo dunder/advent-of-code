4f31cb784991e4d46a6cf251aad704588a4774d3
Matias Jansson <matias.jansson@noretech.se>
2024-12-08 16:23:35 +0100

    Cleanup day 7

--- Solutions/Event2024/Day07.cs
+++ Solutions/Event2024/Day07.cs
@@ -1,11 +1,7 @@
 ﻿using Facet.Combinatorics;
-using Solutions.Event2016.Day11.EnumFlagsSolution;
 using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Runtime.Versioning;
 using Xunit;
 using Xunit.Abstractions;
 using static Solutions.InputReader;
@@ -13,7 +9,7 @@ using static Solutions.InputReader;
 
 namespace Solutions.Event2024
 {
-    // --- Day 7: Phrase ---
+    // --- Day 7: Bridge Repair ---
     public class Day07
     {
         private readonly ITestOutputHelper output;
@@ -23,25 +19,98 @@ namespace Solutions.Event2024
             this.output = output;
         }
 
-        List<(long result, List<int> numbers)> Parse(IList<string> lines)
+        private static List<(long result, List<int> numbers)> Parse(IList<string> lines)
         {
-            List<(long result, List<int> numbers)> input = new();
-
-            foreach (var line in lines)
+            return lines.Select(line =>
             {
-                var parts = line.Split(':', StringSplitOptions.RemoveEmptyEntries);
-                var result = long.Parse(parts[0]);
-                var numbers = parts[1].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
-                input.Add((result, numbers));
-            }
+                string[] parts = line.Split(':', StringSplitOptions.RemoveEmptyEntries);
+                long result = long.Parse(parts[0]);
+                List<int> numbers = parts[1]
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToList();
 
-            return input;
+                return (result, numbers);
+
+            }).ToList();
         }
 
         public enum Operator
         {
             Addition,
-            Multiplication
+            Multiplication,
+            Merge
+        }
+
+        private static long Compute(Dictionary<int, Func<long, int, long>> methods, IList<string> input)
+        {
+            var calibrations = Parse(input);
+
+            var valid = new List<long>();
+
+            var combinationCache = calibrations
+                .Select(c => c.numbers.Count)
+                .Distinct()
+                .Select(count => (count, new Variations<int>(Enumerable.Range(1, methods.Count).ToList(), count, GenerateOption.WithRepetition)))
+                .ToDictionary();
+
+            foreach (var calibration in calibrations)
+            {
+                var totals = new List<long>();
+
+                foreach (var combination in combinationCache[calibration.numbers.Count])
+                {
+                    long first = calibration.numbers.First();
+
+                    var (total, _) = calibration.numbers.Skip(1).Aggregate((first, combination),
+                        (state, number) =>
+                        {
+                            var (total, operators) = state;
+
+                            var op = operators.First();
+
+                            total = methods[op](total, number);
+
+                            return (total, operators.Skip(1).ToList());
+                        });
+
+                    totals.Add(total);
+
+                    if (calibration.result == total)
+                    {
+                        break;
+                    }
+                }
+
+                if (totals.Any(x => x == calibration.result))
+                {
+                    valid.Add(calibration.result);
+                }
+            }
+            return valid.Sum();
+        }
+
+        private static long Problem1(IList<string> input)
+        {
+            var methods = new Dictionary<int, Func<long, int, long>>
+                {
+                    { 1, (x, y) => x + y },
+                    { 2, (x, y) => x * y },
+                };
+
+            return Compute(methods, input);
+        }
+
+        private static long Problem2(IList<string> input)
+        {
+            var methods = new Dictionary<int, Func<long, int, long>>
+                {
+                    { 1, (x, y) => x + y },
+                    { 2, (x, y) => long.Parse(x.ToString() + y) },
+                    { 3, (x, y) => x * y },
+                };
+
+            return Compute(methods, input);
         }
 
         [Fact]
@@ -50,63 +119,7 @@ namespace Solutions.Event2024
         {
             var input = ReadLineInput();
 
-            var calibrations = Parse(input);
-
-            long Execute()
-            {
-                var valid = new List<long>();
-
-                foreach (var calibration in calibrations)
-                {
-                    var binaries = new List<string>();
-                    int length = calibration.numbers.Count-1;
-                    int numbers = 1 << length; // equivalent of 2^n
-
-                    for (int i = 0; i < numbers; i++)
-                    {
-                        string binary = Convert.ToString(i, 2);
-                        string leading_zeroes = "0000000000".Substring(0, length - binary.Length);
-                        binaries.Add(leading_zeroes + binary);
-                    }
-
-                    var totals = new List<long>();
-
-                    foreach (var binary in binaries)
-                    {
-                        long first = calibration.numbers.First();
-
-                        var (total, _) = calibration.numbers.Skip(1).Aggregate((first, binary),
-                            (state, number) =>
-                            {
-                                var (total, operators) = state;
-
-                                var op = operators.First();
-
-                                if (op == '1')
-                                {
-                                    total += number;
-                                }
-                                else
-                                {
-                                    total *= number;
-                                }
-
-
-                                return (total, operators.Substring(1));
-                            });
-
-                        totals.Add(total);
-                    }
-
-                    if (totals.Any(x => x == calibration.result))
-                    {
-                        valid.Add(calibration.result);
-                    }
-                }
-                return valid.Sum();
-            }
-
-            Assert.Equal(-1, Execute());
+            Assert.Equal(1620690235709, Problem1(input));
         }
 
         [Fact]
@@ -115,59 +128,7 @@ namespace Solutions.Event2024
         {
             var input = ReadLineInput();
 
-            var calibrations = Parse(input);
-
-
-            long Execute()
-            {
-                var valid = new List<long>();
-
-                foreach (var calibration in calibrations)
-                {
-                    var combinations = new Variations<int>(new List<int> { 1, 2, 3 }, calibration.numbers.Count, GenerateOption.WithRepetition);
-
-                    var totals = new List<long>();
-
-                    foreach (var combination in combinations)
-                    {
-                        long first = calibration.numbers.First();
-
-                        var (total, _) = calibration.numbers.Skip(1).Aggregate((first, combination),
-                            (state, number) =>
-                            {
-                                var (total, operators) = state;
-
-                                var op = operators.First();
-
-                                if (op == 1)
-                                {
-                                    total += number;
-                                }
-                                else if (op == 2)
-                                {
-                                    var merge = total.ToString() + number;
-                                    total = long.Parse(merge);
-                                }
-                                else
-                                {
-                                    total *= number;
-                                }
-
-                                return (total, operators.Skip(1).ToList());
-                            });
-
-                        totals.Add(total);
-                    }
-
-                    if (totals.Any(x => x == calibration.result))
-                    {
-                        valid.Add(calibration.result);
-                    }
-                }
-                return valid.Sum();
-            }
-
-            Assert.Equal(-1, Execute());
+            Assert.Equal(145397611075341, Problem2(input));
         }
 
         private string exampleText = "";
@@ -188,124 +149,14 @@ namespace Solutions.Event2024
         [Trait("Event", "2024")]
         public void FirstStarExample()
         {
-
-            var calibrations = Parse(exampleInput);
-
-            long Execute()
-            {
-                var valid = new List<long>();
-
-                foreach (var calibration in calibrations)
-                {
-                    var binaries = new List<string>();
-                    int length = 3;
-                    int numbers = 1 << length; // equivalent of 2^n
-
-                    for (int i = 0; i < numbers; i++)
-                    {
-                        string binary = Convert.ToString(i, 2);
-                        string leading_zeroes = "00000000".Substring(0, length - binary.Length);
-                        binaries.Add(leading_zeroes + binary);
-                    }
-
-                    var totals = new List<long>();
-
-                    foreach (var binary in binaries)
-                    {
-                        long first = calibration.numbers.First();
-
-                        var (total, _) = calibration.numbers.Skip(1).Aggregate((first, binary),
-                            (state, number) =>
-                            {
-                                var (total, operators) = state;
-
-                                var op = operators.First();
-
-                                if (op == '1')
-                                {
-                                    total += number;
-                                }
-                                else
-                                {
-                                    total *= number;
-                                }
-
-
-                                return (total, operators.Substring(1));
-                            });
-
-                        totals.Add(total);
-                    }
-
-                    if (totals.Any(x => x == calibration.result))
-                    {
-                        valid.Add(calibration.result);
-                    }
-                }
-                return valid.Sum();
-            }
-
-            Assert.Equal(-1, Execute());
+            Assert.Equal(3749, Problem1(exampleInput));
         }
 
         [Fact]
         [Trait("Event", "2024")]
         public void SecondStarExample()
         {
-
-            var calibrations = Parse(exampleInput);
-
-
-            long Execute()
-            {
-                var valid = new List<long>();
-
-                foreach (var calibration in calibrations)
-                {
-                    var combinations = new Variations<int>(new List<int> { 1, 2, 3 }, 3, GenerateOption.WithRepetition);
-
-                    var totals = new List<long>();
-
-                    foreach (var combination in combinations)
-                    {
-                        long first = calibration.numbers.First();
-
-                        var (total, _) = calibration.numbers.Skip(1).Aggregate((first, combination),
-                            (state, number) =>
-                            {
-                                var (total, operators) = state;
-
-                                var op = operators.First();
-
-                                if (op == 1)
-                                {
-                                    total += number;
-                                }
-                                else if (op == 2)
-                                {
-                                    var merge = total.ToString() + number;
-                                    total = long.Parse(merge);
-                                }
-                                else
-                                {
-                                    total *= number;
-                                }
-
-                                return (total, operators.Skip(1).ToList());
-                            });
-
-                        totals.Add(total);
-                    }
-
-                    if (totals.Any(x => x == calibration.result))
-                    {
-                        valid.Add(calibration.result);
-                    }
-                }
-                return valid.Sum();
-            }
-
-            Assert.Equal(-1, Execute());
+            Assert.Equal(11387, Problem2(exampleInput));
         }
     }
 }
