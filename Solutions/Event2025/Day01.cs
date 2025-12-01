404cd8a8fe5d8003d21496551dc3b199b25116f2
Matias Jansson <matias.jansson@noretech.se>
2025-12-01 06:26:40 +0100

    Solved day 1 of 2025

--- Solutions/Event2025/Day01.cs
+++ Solutions/Event2025/Day01.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 using Xunit.Abstractions;
 using static Solutions.InputReader;
@@ -18,12 +19,49 @@ namespace Solutions.Event2025
 
         private static int Problem1(IList<string> input)
         {
-            return 0;
+            List<(char turn, int count)> instructions = input.Select(row => (row[0], int.Parse(row.Substring(1)))).ToList();
+
+            int count = 0;
+            int dial = 50;
+
+            foreach (var instruction in instructions)
+            {
+                var move = instruction.turn == 'R' ? instruction.count : -instruction.count;
+
+                dial = (dial + move) % 100;
+
+                if (dial == 0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
         }
 
         private static int Problem2(IList<string> input)
         {
-            return 0;
+            List<(char turn, int count)> instructions = input.Select(row => (row[0], int.Parse(row.Substring(1)))).ToList();
+
+            int count = 0;
+            int dial = 50;
+
+            foreach (var instruction in instructions)
+            {
+                var move = instruction.turn == 'R' ? 1 : -1;
+
+                foreach (var _ in Enumerable.Repeat(1, instruction.count))
+                {
+                    dial = (dial + move) % 100;
+
+                    if (dial == 0)
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            return count;
         }
 
         [Fact]
@@ -32,7 +70,7 @@ namespace Solutions.Event2025
         {
             var input = ReadLineInput();
 
-            Assert.Equal(-1, Problem1(input));
+            Assert.Equal(1092, Problem1(input));
         }
 
         [Fact]
@@ -41,7 +79,7 @@ namespace Solutions.Event2025
         {
             var input = ReadLineInput();
 
-            Assert.Equal(-1, Problem2(input));
+            Assert.Equal(6616, Problem2(input));
         }
 
         [Fact]
