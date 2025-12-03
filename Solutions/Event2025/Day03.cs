17414b87e89c18eb63d358fcc5821c8507ea8b98
Matias Jansson <matias.jansson@noretech.se>
2025-12-03 16:48:42 +0100

    Solved day 3 of 2025

--- Solutions/Event2025/Day03.cs
+++ Solutions/Event2025/Day03.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using Xunit;
 using Xunit.Abstractions;
@@ -8,7 +7,7 @@ using static Solutions.InputReader;
 
 namespace Solutions.Event2025
 {
-    // --- Day X: Phrase ---
+    // --- Day 3: Lobby ---
     public class Day03
     {
         private readonly ITestOutputHelper output;
@@ -20,48 +19,91 @@ namespace Solutions.Event2025
 
         private static int Problem1(IList<string> input)
         {
-            return 0;
+            var banks = input.Select(row => row.Select(c => c - '0').ToList());
+
+            var joltage = 0;
+
+            foreach (var bank in banks)
+            {
+                var first = bank.Take(bank.Count - 1);
+
+                var max = first.Max();
+                var right = bank[(bank.IndexOf(max)+1)..];
+
+                var next = right.Max();
+
+                joltage += 10 * max + next;
+            }
+
+            return joltage;
         }
 
-        private static int Problem2(IList<string> input)
+        private static long Problem2(IList<string> input)
         {
-            return 0;
+            var banks = input.Select(row => row.Select(c => c - '0').ToList());
+
+            long joltage = 0;
+
+            foreach (var bank in banks)
+            {
+                List<int> result = [];
+
+                var i = 0;
+
+                while (result.Count < 12)
+                {
+                    var left = bank.Skip(i).ToList();
+                    var check = left.Take(left.Count - (12 - result.Count - 1)).ToList();
+                    var max = check.Max();
+                    var imax = check.IndexOf(max);
+                    result.Add(bank[i + imax]);
+                    i = i + imax + 1;
+                }
+
+                joltage += long.Parse(string.Join("", result));
+            }
+
+            return joltage;
         }
 
         [Fact]
         [Trait("Event", "2025")]
+        [Trait("Event", "2025-03")]
         public void FirstStarTest()
         {
             var input = ReadLineInput();
 
-            Assert.Equal(-1, Problem1(input));
+            Assert.Equal(17408, Problem1(input));
         }
 
         [Fact]
         [Trait("Event", "2025")]
+        [Trait("Event", "2025-03")]
         public void SecondStarTest()
         {
             var input = ReadLineInput();
 
-            Assert.Equal(-1, Problem2(input));
+            Assert.Equal(172740584266849, Problem2(input));
         }
 
         [Fact]
-        [Trait("Example", "2025")]
+        [Trait("Event", "2025")]
+        [Trait("Event", "2025-03")]
         public void FirstStarExample()
         {
             var exampleInput = ReadExampleLineInput("Example");
 
-            Assert.Equal(-1, Problem1(exampleInput));
+            Assert.Equal(357, Problem1(exampleInput));
         }
 
         [Fact]
-        [Trait("Example", "2025")]
+        [Trait("Event", "2025")]
+        [Trait("Event", "2025-03")]
         public void SecondStarExample()
         {
             var exampleInput = ReadExampleLineInput("Example");
 
-            Assert.Equal(-1, Problem2(exampleInput));
+            Assert.Equal(3121910778619, Problem2(exampleInput));
         }
     }
 }
