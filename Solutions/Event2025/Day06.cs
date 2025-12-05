29e0f3d29a000359002da3300f9fbf6e294c1ff4
Matias Jansson <matias.jansson@noretech.se>
2025-12-05 08:38:39 +0100

    Prepare day 6 of 2025

---
+++ Solutions/Event2025/Day06.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using Xunit.Abstractions;
+using static Solutions.InputReader;
+
+
+namespace Solutions.Event2025
+{
+    // --- Day X: Phrase ---
+    public class Day06
+    {
+        private readonly ITestOutputHelper output;
+
+        public Day06(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        private static int Problem1(IList<string> input)
+        {
+            return 0;
+        }
+
+        private static int Problem2(IList<string> input)
+        {
+            return 0;
+        }
+
+        [Fact]
+        [Trait("Event", "2025")]
+        public void FirstStarTest()
+        {
+            var input = ReadLineInput();
+
+            Assert.Equal(-1, Problem1(input));
+        }
+
+        [Fact]
+        [Trait("Event", "2025")]
+        public void SecondStarTest()
+        {
+            var input = ReadLineInput();
+
+            Assert.Equal(-1, Problem2(input));
+        }
+
+        [Fact]
+        [Trait("Example", "2025")]
+        public void FirstStarExample()
+        {
+            var exampleInput = ReadExampleLineInput("Example");
+
+            Assert.Equal(-1, Problem1(exampleInput));
+        }
+
+        [Fact]
+        [Trait("Example", "2025")]
+        public void SecondStarExample()
+        {
+            var exampleInput = ReadExampleLineInput("Example");
+
+            Assert.Equal(-1, Problem2(exampleInput));
+        }
+    }
+}
