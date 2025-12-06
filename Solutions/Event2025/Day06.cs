2c7f0b137474863d4a70adcc786c269e28011ad4
Matias Jansson <matias.jansson@noretech.se>
2025-12-06 07:15:14 +0100

    Solved day 6 of 2025

--- Solutions/Event2025/Day06.cs
+++ Solutions/Event2025/Day06.cs
@@ -8,7 +8,7 @@ using static Solutions.InputReader;
 
 namespace Solutions.Event2025
 {
-    // --- Day X: Phrase ---
+    // --- Day 6: Trash Compactor ---
     public class Day06
     {
         private readonly ITestOutputHelper output;
@@ -18,14 +18,81 @@ namespace Solutions.Event2025
             this.output = output;
         }
 
-        private static int Problem1(IList<string> input)
+        private static long Problem1(IList<string> input)
         {
-            return 0;
+            long total = 0;
+
+            var operations = input.Last().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            List<List<int>> values = [];
+
+            for (int i = 0; i < input.Count - 1; i++)
+            {
+                var lineValues = input[i].Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+                values.Add(lineValues);
+            }
+
+            for (int column = 0; column < operations.Count; column++)
+            {
+                var operation = operations[column];
+                long subTotal = operation == "+" ? 0 : 1;
+
+                for (int row = 0; row < values.Count; row++)
+                {
+                    var columnValue = values[row][column];
+
+                    subTotal = operation == "*" ? subTotal * columnValue : subTotal + columnValue;
+
+                }
+                total += subTotal;
+            }
+
+            return total;
         }
 
-        private static int Problem2(IList<string> input)
+        private static long Problem2(IList<string> input)
         {
-            return 0;
+            string operationsRow = input.Last();
+            var operations = operationsRow.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            List<int> operationIndexes = [];
+
+            int depth = input.Count - 1;
+            int width = input.First().Length;
+            List<string> values = Enumerable.Repeat("", width).ToList();
+
+            for (int row = 0; row < input.Count - 1; row++)
+            {
+                for (int column = 0; column < width; column++)
+                {
+                    var value = input[row][column] == ' ' ? "" : input[row][column].ToString();
+                    values[column] += value ;
+                }
+            }
+
+            long total = 0;
+            long subTotal = operations.First() == "+" ? 0 : 1;
+            var operation = operations[0];
+            int oi = 0;
+
+            for (int i = 0; i < values.Count;i++)
+            {
+                if (values[i] == "")
+                {
+                    total += subTotal;
+                    oi++;
+                    operation = operations[oi];
+                    subTotal = operation == "+" ? 0 : 1;
+                    continue;
+                }
+
+                var columnValue = int.Parse(values[i]);
+
+                subTotal = operation == "*" ? subTotal * columnValue : subTotal + columnValue;
+            }
+
+            return total + subTotal;
         }
 
         [Fact]
@@ -34,7 +101,7 @@ namespace Solutions.Event2025
         {
             var input = ReadLineInput();
 
-            Assert.Equal(-1, Problem1(input));
+            Assert.Equal(5524274308182, Problem1(input));
         }
 
         [Fact]
@@ -43,7 +110,7 @@ namespace Solutions.Event2025
         {
             var input = ReadLineInput();
 
-            Assert.Equal(-1, Problem2(input));
+            Assert.Equal(8843673199391, Problem2(input));
         }
 
         [Fact]
@@ -52,7 +119,7 @@ namespace Solutions.Event2025
         {
             var exampleInput = ReadExampleLineInput("Example");
 
-            Assert.Equal(-1, Problem1(exampleInput));
+            Assert.Equal(4277556, Problem1(exampleInput));
         }
 
         [Fact]
@@ -61,7 +128,7 @@ namespace Solutions.Event2025
         {
             var exampleInput = ReadExampleLineInput("Example");
 
-            Assert.Equal(-1, Problem2(exampleInput));
+            Assert.Equal(3263827, Problem2(exampleInput));
         }
     }
 }
