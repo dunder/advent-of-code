997910f9673c158a605f123179e7d0a11d047357
Matias Jansson <matias.jansson@noretech.se>
2025-12-07 09:36:13 +0100

    Solved day 7 of 2025

--- Solutions/Event2025/Day07.cs
+++ Solutions/Event2025/Day07.cs
@@ -1,3 +1,5 @@
+﻿using MoreLinq.Extensions;
+using Shared.Tree;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,14 +20,179 @@ namespace Solutions.Event2025
             this.output = output;
         }
 
-        private static int Problem1(IList<string> input)
+        private static (char[,] map, (int x, int y) start, HashSet<(int x, int y)> splitters) ParseMap(IList<string> input)
         {
-            return 0;
+            int maxx = input.First().Length;
+            int maxy = input.Count;
+
+            char[,] map = new char[maxx, maxy];
+            HashSet<(int x, int y)> splitters = [];
+            (int x, int y) start = (0,0);
+            for (var y = 0; y < maxy; y++)
+            {
+                var line = input[y];
+
+                for (var x = 0; x < maxx; x++)
+                {
+                    var c = line[x];
+
+                    map[x, y] = c;
+
+                    if (c == 'S')
+                    {
+                        start = (x, y);
+                    }
+                    else if (c == '^')
+                    {
+                        splitters.Add((x, y));
+                    }
+
+                }
+            }
+
+            return (map, start, splitters);
         }
 
-        private static int Problem2(IList<string> input)
+        private static List<List<(int x, int y)>> ParseMap2(IList<string> input)
         {
-            return 0;
+            int maxx = input.First().Length;
+            int maxy = input.Count;
+
+            List<List<int>> splitters = [];
+
+            for (var y = 1; y < maxy; y++)
+            {
+                var line = input[y];
+
+                List<int> lineSplitters = line.Where((c, i) => c == '^').Select((c, i) => i).ToList();
+
+                if (lineSplitters.Any())
+                {
+                    splitters.Add(lineSplitters);
+                }
+            }
+
+            return splitters.Select((line, y) => line.Select(x => (x, y)).ToList()).ToList();
+        }
+
+        private static int Problem1(IList<string> input)
+        {
+            var (map, start, splitters) = ParseMap(input);
+
+            HashSet<(int x, int y)> beams = [start];
+
+            var count = 0;
+
+            for (int t = 1; t < map.GetLength(1); t++)
+            {
+                HashSet<(int x, int y)> newBeams = [];
+
+                foreach (var beam in beams)
+                {
+                    (int x, int y) location = (beam.x, beam.y);
+
+                    if (splitters.Contains(location))
+                    {
+                        newBeams.Add((location.x-1, location.y+1));
+                        newBeams.Add((location.x+1, location.y+1));
+                        count++;
+                    }
+                    else
+                    {
+                        newBeams.Add((location.x, location.y + 1));
+                    }
+                }
+
+                beams = newBeams;
+            }
+
+            return count;
+        }
+
+        private record Splitter(int X, int Y);
+
+        private class Node
+        {
+            public Node((int x, int y) splitter)
+            {
+                Splitter = new Splitter(splitter.x, splitter.y);
+            }
+            public Splitter Splitter { get; set; }
+            public Splitter Right { get; set; }
+            public Splitter Left { get; set; }
+
+            public override string ToString()
+            {
+                return $"Left({Left}) Right({Right})";
+            }
+        }
+
+        private static long Problem2(IList<string> input)
+        {
+            var (map, _, splitters) = ParseMap(input);
+
+            Dictionary<Splitter, Node> nodes = splitters.ToDictionary(s => new Splitter(s.x, s.y), s => new Node(s));
+
+            foreach (var splitter in splitters.Select(s => new Splitter(s.x, s.y)).ToList())
+            {
+                var right = splitters.Where(s => s.x == splitter.X + 1).Where(s => s.y > splitter.Y).OrderBy(s => s.y);
+                var left = splitters.Where(s => s.x == splitter.X - 1).Where(s => s.y > splitter.Y).OrderBy(s => s.y);
+
+                if (right.Any())
+                {
+                    var rightSplitter = right.First();
+                    nodes[splitter].Right = new Splitter(rightSplitter.x, rightSplitter.y);
+                }
+
+                if (left.Any())
+                {
+                    var leftSplitter = left.First();
+                    nodes[splitter].Left = new Splitter(leftSplitter.x, leftSplitter.y);
+                }
+            }
+
+            var root = nodes.Where(n => n.Key.Y == 2).Single().Value;
+
+            Dictionary<Splitter, long> cache = [];
+
+            long Count(Node root)
+            {
+                if (root == null)
+                {
+                    return 1;
+                }
+
+                if (cache.ContainsKey(root.Splitter))
+                {
+                    return cache[root.Splitter];
+                }
+
+                if (root.Right == null && root.Left == null)
+                {
+                    return 2;
+                }
+
+                var left = root.Left == null ? null : nodes[root.Left];
+                var right = root.Right == null ? null : nodes[root.Right];
+
+                var leftCount = Count(left);
+
+                if (left != null && !cache.ContainsKey(left.Splitter))
+                {
+                    cache.Add(left.Splitter, leftCount);
+                }
+
+                var rightCount = Count(right);
+
+                if (right != null && !cache.ContainsKey(right.Splitter))
+                {
+                    cache.Add(right.Splitter, rightCount);
+                }
+
+                return leftCount + rightCount;
+            }
+
+            return Count(root);
         }
 
         [Fact]
@@ -34,7 +201,7 @@ namespace Solutions.Event2025
         {
             var input = ReadLineInput();
 
-            Assert.Equal(-1, Problem1(input));
+            Assert.Equal(1573, Problem1(input));
         }
 
         [Fact]
@@ -43,7 +210,7 @@ namespace Solutions.Event2025
         {
             var input = ReadLineInput();
 
-            Assert.Equal(-1, Problem2(input));
+            Assert.Equal(15093663987272, Problem2(input));
         }
 
         [Fact]
@@ -52,7 +219,7 @@ namespace Solutions.Event2025
         {
             var exampleInput = ReadExampleLineInput("Example");
 
-            Assert.Equal(-1, Problem1(exampleInput));
+            Assert.Equal(21, Problem1(exampleInput));
         }
 
         [Fact]
@@ -61,7 +228,7 @@ namespace Solutions.Event2025
         {
             var exampleInput = ReadExampleLineInput("Example");
 
-            Assert.Equal(-1, Problem2(exampleInput));
+            Assert.Equal(40, Problem2(exampleInput));
         }
     }
 }
