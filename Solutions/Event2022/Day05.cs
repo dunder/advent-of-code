f9bc527effb89c8de6b2a36c6ec7c690815d3901
Matias Jansson <matias.jansson@noretech.se>
2025-12-07 16:23:18 +0100

    Solved day 5 2022 with C#. Remove unused template.

--- Solutions/Event2022/Day05.cs
+++ Solutions/Event2022/Day05.cs
@@ -19,24 +19,83 @@ namespace Solutions.Event2022
             this.output = output;
         }
 
-        private static int Problem1(IList<string> input)
+        private static Dictionary<int, Stack<char>> CreateStacks(List<string> stackInput)
         {
-            List<string> stacks = input.Split("").First().ToList();
-
-            int stackCount = stacks
+            int stackCount = stackInput
                 .Last()
                 .Split(" ", StringSplitOptions.RemoveEmptyEntries)
                 .Select(int.Parse)
                 .Max();
 
-            List<string> moves = input.Split("").Last().ToList();
+            Dictionary<int, Stack<char>> stacks = Enumerable.Range(1, stackCount)
+                .Select(i => (i, new Stack<char>()))
+                .ToDictionary();
 
-            return 0;
+            foreach (var column in Enumerable.Range(1, stackCount))
+            {
+                int stackIndex = stackInput.Last().IndexOf(column.ToString());
+
+                for (int i = stackInput.Count - 2; i >= 0; i--)
+                {
+                    var crates = stackInput[i];
+                    var crate = crates[stackIndex];
+
+                    if (char.IsLetter(crate))
+                    {
+                        stacks[column].Push(crate);
+                    }
+                }
+            }
+
+            return stacks;
         }
 
-        private static int Problem2(IList<string> input)
+        private static string Problem1(IList<string> input)
         {
-            return 0;
+            List<string> stackInput = input.Split("").First().ToList();
+            List<string> moves = input.Split("").Last().ToList();
+
+            Dictionary<int, Stack<char>> stacks = CreateStacks(stackInput);
+
+            foreach (var move in moves)
+            {
+                var values = move.Split(" ").Where(w => int.TryParse(w, out _)).Select(int.Parse).ToList();
+                var times = values[0];
+                var from = values[1];
+                var to = values[2];
+
+                foreach (var _ in Enumerable.Range(0, times))
+                {
+                    stacks[to].Push(stacks[from].Pop());
+                }
+            }
+
+            return string.Join("", stacks.Keys.Select(i => stacks[i].Pop()));
+        }
+
+        private static string Problem2(IList<string> input)
+        {
+            List<string> stackInput = input.Split("").First().ToList();
+            List<string> moves = input.Split("").Last().ToList();
+
+            Dictionary<int, List<char>> stacks = CreateStacks(stackInput)
+                .Select(kvp => (kvp.Key, new List<char>(kvp.Value)))
+                .ToDictionary();
+
+            foreach (var move in moves)
+            {
+                var values = move.Split(" ").Where(w => int.TryParse(w, out _)).Select(int.Parse).ToList();
+                var times = values[0];
+                var from = values[1];
+                var to = values[2];
+
+                var moved = stacks[from].Take(times).ToList();
+                stacks[from] = stacks[from].Skip(times).ToList();
+                moved.AddRange(stacks[to]);
+                stacks[to] = moved;
+            }
+
+            return string.Join("", stacks.Keys.Select(i => stacks[i][0]));
         }
 
         [Fact]
@@ -45,7 +104,7 @@ namespace Solutions.Event2022
         {
             var input = ReadLineInput();
 
-            Assert.Equal(-1, Problem1(input));
+            Assert.Equal("QNNTGTPFN", Problem1(input));
         }
 
         [Fact]
@@ -54,7 +113,7 @@ namespace Solutions.Event2022
         {
             var input = ReadLineInput();
 
-            Assert.Equal(-1, Problem2(input));
+            Assert.Equal("GGNPJBTTR", Problem2(input));
         }
 
         [Fact]
@@ -63,7 +122,7 @@ namespace Solutions.Event2022
         {
             var exampleInput = ReadExampleLineInput("Example");
 
-            Assert.Equal(-1, Problem1(exampleInput));
+            Assert.Equal("CMZ", Problem1(exampleInput));
         }
 
         [Fact]
@@ -72,7 +131,7 @@ namespace Solutions.Event2022
         {
             var exampleInput = ReadExampleLineInput("Example");
 
-            Assert.Equal(-1, Problem2(exampleInput));
+            Assert.Equal("MCD", Problem2(exampleInput));
         }
     }
 }
